4f71e070970086f08437fdefb6eb32333094acb5
Tom Payne <thomas.payne@skybettingandgaming.com>
2017-03-14 16:29:12 +0000

    Removed old fish shell config

--- fish-shell/functions/__my_fish_separator.fish
+++
@@ -1,5 +0,0 @@
-function __my_fish_separator --description 'Gets a coloured separator between segments of prompt'
-    set -l __my_fish_segment_separator '⮀'
-    set __fish_my_separator (set_color --background=$argv[1] $argv[2])
-    echo "$__fish_my_separator"$__my_fish_segment_separator
-end
