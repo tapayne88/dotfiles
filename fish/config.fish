fab8921a2dacf766babe52b512aad0c2ab922c29
Tom Payne <thomas.payne@skybettingandgaming.com>
2017-03-15 15:14:37 +0000

    Added fish shell config with base16 colors

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+set -g theme_short_path yes
+
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-materia.sh
+end
+
+export VISUAL=nvim
+export EDITOR=nvim
+
+# executable search path
+set PATH $HOME/.local/bin $HOME/.local/sbin /usr/local/bin $PATH
+
+# Make some possibly destructive commands more interactive
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
+
+alias grep='grep -n --color=auto'
+alias ..='cd ..'
+
+alias fem='cd $HOME/dev/stash.skybet.net/skybetdev/tiny/Mbet/React/FootballEventMarkets'
+alias uil='cd $HOME/dev/stash.skybet.net/@skybet/ui-library'
+alias sbd='cd $HOME/dev/stash.skybet.net/skybetdev'
+
+# For reviewing merge before commit
+# Review flow below:
+#   - git mg --no-ff --no-commit <branch>
+#   - vimmg <any file currently staged>
+#   - :Gdiff ~1 (vim - compare staged copy with repo version)
+#   - swap back to Gstatus pane
+#   - move cursor to new file and press <Enter> (opens file below status)
+alias vimmg='vim -c Gstatus'
---
+++ fish/functions/tm.fish
@@ -0,0 +1,39 @@
+# Idempotent tmux startup function
+function tm
+    if test "$argv[1]" = "-h"
+        echo "tm - My handy function for managing tmux sessions"
+        echo ""
+        echo "Usage:"
+        echo "   tm <flags> <args>"
+        echo ""
+        echo "Flags:"
+        echo "   -h      Help, print his help message"
+        echo "   -k,-d   Delete named session (<args>)"
+        echo "   -l      List sessions"
+        return
+    end
+
+    # destroy flag
+    if test "$argv[1]" = "-k" -o "$argv[1]" = "-d"
+        tmux kill-session -t $argv[2]
+        return
+    end
+
+    # list flag
+    if test "$argv[1]" = "-l"
+        tmux list-session
+        return
+    end
+
+    tmux has-session -t $argv[1] > /dev/null 2>&1
+
+    if test $status -ne 0
+        set TMUX tmux new-session -d -s $argv[1] -c "$HOME"
+    end
+
+    if test -n "$TMUX"
+        tmux switch -t $argv[1]
+    else
+        tmux attach -t $argv[1]
+    end
+end
