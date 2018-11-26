2e2a831668e71c15ec078cfc3883f6b6c78abb46
Tom Payne <thomas.payne@skybettingandgaming.com>
2018-11-26 19:44:52 +0000

    Added serverless autocomplete

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -78,3 +78,10 @@ set -l LOCAL_FISH ~/.local/config.fish
 if test -e $LOCAL_FISH
     source $LOCAL_FISH
 end
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
