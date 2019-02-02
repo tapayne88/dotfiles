f9d05d1e1a74defbf78383fc97be2ea376f10b1e
Tom Payne <thomas.payne@skybettingandgaming.com>
2019-02-02 18:01:55 +0000

    Script to log out fish colors

---
+++ scripts/echo_fish_colors.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+echo "fish_color_normal "$fish_color_normal
+echo "fish_color_command "$fish_color_command
+echo "fish_color_quote "$fish_color_quote
+echo "fish_color_redirection "$fish_color_redirection
+echo "fish_color_end "$fish_color_end
+echo "fish_color_error "$fish_color_error
+echo "fish_color_param "$fish_color_param
+echo "fish_color_comment "$fish_color_comment
+echo "fish_color_match "$fish_color_match
+echo "fish_color_selection "$fish_color_selection
+echo "fish_color_search_match "$fish_color_search_match
+echo "fish_color_operator "$fish_color_operator
+echo "fish_color_escape "$fish_color_escape
+echo "fish_color_cwd "$fish_color_cwd
+echo "fish_color_autosuggestion "$fish_color_autosuggestion
+echo "fish_color_user "$fish_color_user
+echo "fish_color_host "$fish_color_host
+echo "fish_color_cancel "$fish_color_cancel
+echo "fish_pager_color_prefix "$fish_pager_color_prefix
+echo "fish_pager_color_completion "$fish_pager_color_completion
+echo "fish_pager_color_description "$fish_pager_color_description
+echo "fish_pager_color_progress "$fish_pager_color_progress
+echo "fish_pager_color_secondary "$fish_pager_color_secondary
