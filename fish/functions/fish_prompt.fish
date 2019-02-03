9e633ccc9499a48222dc2fb4ef1f7588bce72ff3
Tom Payne <thomas.payne@skybettingandgaming.com>
2019-02-03 19:59:45 +0000

    Updated fish prompt git icons

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,9 +5,9 @@ function fish_prompt
   set -l fish     "❯"
   set -l ahead    "⇡"
   set -l behind   "⇣"
-  set -l diverged "⇡⇣"
-  set -l dirty    "✎ "
-  set -l none     "◦"
+  set -l diverged "⇕"
+  set -l dirty    "⋯ "
+  set -l none     "•"
 
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
