6e4d8af99f976eb27377f7bc34817bfc9989493e
Tom Payne <thomas.payne@skybettingandgaming.com>
2019-12-12 17:32:12 +0000

    Made docker scripts work across os's

--- zsh/plugins/docker.zsh
+++ zsh/plugins/docker.zsh
@@ -1,9 +1,31 @@
 # Interactively remove docker containers
 function docker_rm() {
-  docker ps -a | fzf --multi --header-lines=1 | awk '{ print $1 }' | xargs docker rm -f
+  # Do we need to sudo?
+  docker ps > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    # If we need to sudo then we're not on mac, therefore we likely have GNU tools not BSD,
+    # hence the --no-run-if-empty (which BSD seems to have by default)
+    command="sudo docker"
+    xargs="xargs --no-run-if-empty"
+  else
+    command="docker"
+    xargs="xargs"
+  fi
+  eval "$command ps -a" | fzf --height 15 --multi --header-lines=1 | awk '{ print $1 }' | eval "$xargs $command rm -f"
 }
 
 # Interactively remove docker images
 function docker_rmi() {
-  docker images -a | fzf --multi --header-lines=1 | awk '{ print $3 }' | xargs docker rmi -f
+  # Do we need to sudo?
+  docker ps > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    # If we need to sudo then we're not on mac, therefore we likely have GNU tools not BSD,
+    # hence the --no-run-if-empty (which BSD seems to have by default)
+    command="sudo docker"
+    xargs="xargs --no-run-if-empty"
+  else
+    command="docker"
+    xargs="xargs"
+  fi
+  eval "$command images -a" | fzf --height 15 --multi --header-lines=1 | awk '{ print $3 }' | eval "$xargs $command rmi -f"
 }
