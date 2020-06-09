f98e6163561e44a09e5b28788f22404884d7b1a5
Tom Payne <tpayne@fastmail.co.uk>
2020-06-09 20:19:42 +0100

    Fixed install script for systems without chezmoi config dir

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 CWD=`pwd`
 INSTALL_LOCATION="$CWD/dotfiles"
 REPO="git@github.com:tapayne88/dotfiles.git"
-CHEZMOI_CONFIG_LOCATION="$HOME/.config/chezmoi/chezmoi.json"
+CHEZMOI_CONFIG_DIR="$HOME/.config/chezmoi"
+CHEZMOI_CONFIG_FILE="$CHEZMOI_CONFIG_DIR/chezmoi.json"
 CHEZMOI_CONFIG="{
   \"sourceDir\": \"$INSTALL_LOCATION\",
   \"merge\": {
@@ -23,14 +24,15 @@ echo "Cloning $REPO to $INSTALL_LOCATION"
 git clone $REPO $INSTALL_LOCATION
 chmod 700 $INSTALL_LOCATION
 
-if [ -f "$CHEZMOI_CONFIG_LOCATION" ]; then
-  echo "Found $CHEZMOI_CONFIG_LOCATION, halting"
+if [ -f "$CHEZMOI_CONFIG_FILE" ]; then
+  echo "Found $CHEZMOI_CONFIG_FILE, halting"
   echo "Merge config with current file"
   echo "$CHEZMOI_CONFIG"
   exit 1
 fi
 
-echo "$CHEZMOI_CONFIG" > $CHEZMOI_CONFIG_LOCATION
+mkdir -p $CHEZMOI_CONFIG_DIR
+echo "$CHEZMOI_CONFIG" > $CHEZMOI_CONFIG_FILE
 
 echo ""
 echo "Next stps:
