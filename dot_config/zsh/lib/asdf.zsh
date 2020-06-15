658250704867593c5cbb5ab798c4a52cec58a5d8
Tom Payne <tom@tompayne.dev>
2020-06-15 11:39:09 +0100

    Alphabetised zsh plugins, added official asdf plugin and remove own

--- dot_config/zsh/lib/asdf.zsh
+++
@@ -1,4 +0,0 @@
-if type "asdf" > /dev/null; then
-  source `dirname $(dirname $(which brew))`/opt/asdf/asdf.sh
-  source `dirname $(dirname $(which brew))`/opt/asdf/etc/bash_completion.d/asdf.bash
-fi
