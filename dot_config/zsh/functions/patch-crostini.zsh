45cfb7c87e3ba1544393a2ab6ce8eae3cb27482b
Tom Payne <tom@tompayne.dev>
2020-10-02 19:33:11 +0100

    Added patching functions to zsh to get nix and alacritty working

---
+++ dot_config/zsh/functions/patch-crostini.zsh
@@ -0,0 +1,12 @@
+function patch-alacritty {
+  sudo mkdir -p /run/opengl-driver
+  sudo ln -s `nix eval --raw nixpkgs.mesa_drivers.outPath`/lib /run/opengl-driver/lib
+}
+
+function patch-nix {
+  sudo umount /proc/{cpuinfo,diskstats,meminfo,stat,uptime}
+}
+
+function launch-alacritty {
+  WAYLAND_DISPLAY= alacritty &
+}
