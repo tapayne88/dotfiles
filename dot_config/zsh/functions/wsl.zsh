6e30bf6f6860b62e6691f04a08b3e4da28ebc4b6
Tom Payne <tom@tompayne.dev>
2020-10-31 16:17:30 +0000

    Added some zsh functions to ease WSL chezmoi provisioning

---
+++ dot_config/zsh/functions/wsl.zsh
@@ -0,0 +1,7 @@
+function copy-alacritty-config {
+  cp ~/.config/alacritty/alacritty.yml /mnt/c/Users/tapay/AppData/Roaming/alacritty
+}
+
+function copy-windows-terminal-config {
+  cp ~/.config/windows_terminal/settings.json /mnt/c/Users/tapay/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+}
