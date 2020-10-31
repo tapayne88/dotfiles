291e14044f6cad667ee478086dd67c863e164632
Tom Payne <tom@tompayne.dev>
2020-10-31 16:50:04 +0000

    Added diffing to wsl provision command for alacritty and window terminal

--- dot_config/zsh/functions/wsl.zsh
+++ dot_config/zsh/functions/wsl.zsh
@@ -1,7 +1,19 @@
 function copy-alacritty-config {
-  cp ~/.config/alacritty/alacritty.yml /mnt/c/Users/tapay/AppData/Roaming/alacritty
+  fake_chezmoi "$1" ~/.config/alacritty/alacritty.yml /mnt/c/Users/tapay/AppData/Roaming/alacritty
 }
 
 function copy-windows-terminal-config {
-  cp ~/.config/windows_terminal/settings.json /mnt/c/Users/tapay/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+  fake_chezmoi "$1" ~/.config/windows_terminal/settings.json /mnt/c/Users/tapay/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+}
+
+function fake_chezmoi {
+  local arg=$1
+  local new_settings=$2
+  local current_settings=$3
+
+  diff -u $current_settings $new_settings | diff-so-fancy
+
+  if [ "$arg" != "-n" ]; then
+    cp $new_settings $current_settings
+  fi
 }
