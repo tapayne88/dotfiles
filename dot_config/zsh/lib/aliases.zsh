ec5d07f4dedfbbf05bdbd1d3a2bc9662374d0cbe
Tom Payne <tom@tompayne.dev>
2020-10-31 15:49:24 +0000

    Added open alias for wsl to open current folder and wrapped macOS ls hack with platform detection

--- dot_config/zsh/lib/aliases.zsh
+++
@@ -1,6 +0,0 @@
-# Register gls alias to trick oh-my-zsh into thinking we've installed coreutils
-# through homebrew (actually installed with nix). This way we get nice ls color
-# support
-alias gls='ls'
-
-alias ssh='TERM=xterm-256color ssh'
