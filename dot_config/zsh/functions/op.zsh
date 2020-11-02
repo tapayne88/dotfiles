87c9eb6c931003454856b575fa20febd61f11c11
Tom Payne <tom@tompayne.dev>
2020-11-02 15:58:31 +0000

    Fixed zsh 1password helper functions - not sure how they every worked

--- dot_config/zsh/functions/op.zsh
+++ dot_config/zsh/functions/op.zsh
@@ -1,14 +1,14 @@
-local OP_TMUX_TOKEN_FILE="$HOME/.op_tmux_token_tmp"
+OP_TMUX_TOKEN_FILE="$HOME/.op_tmux_token_tmp"
 
 function sourceOPSession {
-  if (test -n "$OP_TMUX_TOKEN_FILE" && test -e "$OP_TMUX_TOKEN_FILE"); then
+  if [[ -n "$OP_TMUX_TOKEN_FILE" && -e "$OP_TMUX_TOKEN_FILE" ]]; then
     export OP_SESSION_my="$(cat $OP_TMUX_TOKEN_FILE)"
   fi
 }
 
 function opsignin {
   sourceOPSession
-  if (! test -e $OP_TMUX_TOKEN_FILE); then
+  if [[ ! -e $OP_TMUX_TOKEN_FILE ]]; then
     op signin my --output=raw | tee $OP_TMUX_TOKEN_FILE
     sourceOPSession
   fi
