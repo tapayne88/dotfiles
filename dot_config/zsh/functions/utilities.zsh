66a91027387f66f2256f228dc03a2f83d9bee27b
Tom Payne <tom@tompayne.dev>
2021-01-23 12:32:26 +0000

    Swapped ssh alias for wrapper function borrowed (and slightly modified from Greg Hurrell - couldn't get `command` to work)

---
+++ dot_config/zsh/functions/utilities.zsh
@@ -0,0 +1,9 @@
+# Catch-all for simple utility functions
+
+# ensure ssh server has terminfo for env TERM
+function ssh() {
+  emulate -L zsh
+
+  local LOCAL_TERM=$(echo -n "$TERM" | sed -e s/tmux/screen/)
+  env TERM=$LOCAL_TERM ssh "$@"
+}
