f128663542b15cf1f59cca07e83017abce21b39d
Tom Payne <tom@tompayne.dev>
2021-02-11 17:18:37 +0000

    Extracted lua config to separate lua files

---
+++ dot_config/nvim/lua/_treesitter.lua
@@ -0,0 +1,12 @@
+require "nvim-treesitter.configs".setup {
+  ensure_installed = "all",
+  highlight = {
+    enable = true
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false -- Whether the query persists across vim sessions
+  }
+}
