99636ea3773890ec9ce837718c20640707d04a13
Tom Payne <tom@tompayne.dev>
2021-03-06 11:05:08 +0000

    Added nvim-compe for lsp auto-completion, basic configuration

--- dot_config/nvim/lua/_lsp.lua
+++ dot_config/nvim/lua/_lsp.lua
@@ -21,6 +21,34 @@ saga.init_lsp_saga({
   border_style = 2
 })
 
+require('compe').setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 200;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    vsnip = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    spell = true;
+    tags = true;
+    snippets_nvim = true;
+    treesitter = true;
+  };
+}
+
 local on_attach = function(client, bufnr)
 
   lsp_status.on_attach(client, bufnr)
