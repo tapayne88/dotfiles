70dc9374f0d5338084a4cc46ccc150a98711729e
Tom Payne <tom@tompayne.dev>
2021-04-15 16:27:13 +0100

    Added function to convert git repo to git worktree

--- dot_config/zsh/functions/gw.zsh
+++ dot_config/zsh/functions/gw.zsh
@@ -40,3 +40,48 @@ function gw() {
     cd "$WORKTREE_DIR/$GIT_PREFIX"
   fi
 }
+
+function git-worktree-init() {
+  if [ "$1" = "-h" ]; then
+    echo "git-worktree-init - convert a git repo to worktree setup"
+    echo ""
+    echo "Usage:"
+    echo "   gw <flags>"
+    echo ""
+    echo "Flags:"
+    echo "   -h     Help, print his help message"
+    echo ""
+    echo "\`git-worktree-init\` will convert the current git repo to a worktree."
+    return
+  fi
+
+  if [ ! -d ".git" ]; then
+    echo "not in root of git repo"
+    return 1
+  fi
+
+  REPO_DIR=$(pwd)
+  PARENT_DIR=$(dirname $REPO_DIR)
+  REPO_NAME=$(basename $REPO_DIR)
+
+  TEMP_REPO="/tmp/$REPO_NAME"
+
+  echo "checking out default branch"
+  DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+  git checkout $DEFAULT_BRANCH > /dev/null 2>&1
+  
+  # Remove old location files
+  echo "moving current repo to /tmp"
+  cd ..
+  mv $REPO_NAME $TEMP_REPO
+
+  # Make new directory structure
+  echo "setting up new directory structure"
+  mkdir -p "$REPO_NAME/worktrees"
+  cp -r "$TEMP_REPO/.git" "$REPO_NAME/.$REPO_NAME.git"
+
+  # Setup hidden actual repo as bare to avoid accidentally checking out a branch
+  echo "setting hidden repo as bare"
+  cd "$REPO_NAME/.$REPO_NAME.git"
+  git config --bool core.bare true
+}
