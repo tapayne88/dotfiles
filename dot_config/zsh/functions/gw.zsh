5e86c5b5c5a76b981ca73ac543619ccdbce96ee4
Tom Payne <tom@tompayne.dev>
2021-04-15 15:14:42 +0100

    Expanded git worktree command to be able to pass worktree name when running

--- dot_config/zsh/functions/gw.zsh
+++ dot_config/zsh/functions/gw.zsh
@@ -3,14 +3,13 @@ function gw() {
     echo "gw - quick way to swap between git worktrees and remain in the same subdirectory, using fzf"
     echo ""
     echo "Usage:"
-    echo "   gw <flags>"
-    # echo "   gw <flags> <worktree name>"
+    echo "   gw <flags> <worktree_name>"
     echo ""
     echo "Flags:"
     echo "   -h     Help, print his help message"
     echo ""
     echo "\`gw\` will show available git worktrees via fzf."
-    # echo "\`gw master\` will swap to the master worktree in the same subdirectory."
+    echo "\`gw master\` will swap to the master worktree in the same subdirectory."
     return
   fi
 
@@ -20,11 +19,24 @@ function gw() {
   fi
 
   GIT_PREFIX=$(git rev-parse --show-prefix)
-  WORKTREE_DIR=$(git worktree list | fzf --height 15 --header "swap worktree" | awk '{ print $1 }')
 
-  if [ "$WORKTREE_DIR" = "" ]; then
+  if [ "$1" != "" ]; then
+    for WT in $(git worktree list | awk '{ print $1 }'); do
+      if [ "$(basename $WT)" = "$1" ]; then
+        cd "$WT/$GIT_PREFIX"
+        return
+      fi
+    done
+
+    echo "no worktree found called $1"
     return 1
-  fi
+  else
+    WORKTREE_DIR=$(git worktree list | fzf --height 15 --header "swap worktree" | awk '{ print $1 }')
 
-  cd "$WORKTREE_DIR/$GIT_PREFIX"
+    if [ "$WORKTREE_DIR" = "" ]; then
+      return 1
+    fi
+
+    cd "$WORKTREE_DIR/$GIT_PREFIX"
+  fi
 }
