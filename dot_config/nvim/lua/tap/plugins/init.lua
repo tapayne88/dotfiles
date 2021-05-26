085e6201b3447cf8db2021f0ef58b4c2a81e3cd2
Tom Payne <tom@tompayne.dev>
2021-05-26 19:54:43 +0100

    Added vim-matchup for improved % handling

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -38,6 +38,15 @@ return require('packer').startup(function(use)
     use 'nvim-lua/plenary.nvim'                         -- Utility function used by plugins and my config
     -- LuaFormatter on
 
+    -- even better % navigation
+    use {
+        'andymass/vim-matchup',
+        config = function()
+            vim.g.matchup_surround_enabled = 1
+            vim.g.matchup_matchparen_offscreen = {method = 'popup'}
+        end
+    }
+
     -- Filetype icon support with lua support
     use {'ryanoasis/vim-devicons', 'kyazdani42/nvim-web-devicons'}
 
--- dot_config/nvim/lua/tap/plugins/treesitter.lua
+++ dot_config/nvim/lua/tap/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 require"nvim-treesitter.configs".setup {
     ensure_installed = "maintained",
     highlight = {enable = true},
+    matchup = {enable = true},
     playground = {
         enable = true,
         disable = {},
