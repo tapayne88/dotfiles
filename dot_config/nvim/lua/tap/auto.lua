68cba9c75e0fced87147e2becbf80897720886b8
Tom Payne <tom@tompayne.dev>
2021-06-20 19:24:20 +0100

    Converted autocommands to use lua

--- dot_config/nvim/lua/tap/auto.lua
+++ dot_config/nvim/lua/tap/auto.lua
@@ -1,8 +1,18 @@
-vim.cmd [[
-" Automatically resize vim splits on resize
-autocmd VimResized * execute "normal! \<c-w>="
+local augroup = require("tap.utils").augroup
+local termcodes = require("tap.utils").termcodes
 
-" Save and load vim views - remembers scroll position & folds
-autocmd BufWinLeave *.* mkview
-autocmd BufWinEnter *.* silent! loadview
-]]
+-- Automatically resize vim splits on resize
+augroup("TapWinResize", {
+    {
+        events = {"VimResized"},
+        targets = {"*"},
+        command = 'execute "normal! ' .. termcodes("<c-w>") .. '="'
+
+    }
+})
+
+-- Save and load vim views - remembers scroll position & folds
+augroup("TapMkViews", {
+    {events = {"BufWinLeave"}, targets = {"*.*"}, command = 'mkview'},
+    {events = {"BufWinEnter"}, targets = {"*.*"}, command = 'silent! loadview'}
+})
