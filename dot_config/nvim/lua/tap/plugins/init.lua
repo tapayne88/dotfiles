878c2695e4f674539348a726828006ed5c0305e4
Tom Payne <tom@tompayne.dev>
2021-06-28 16:38:01 +0100

    Moved dashboard-nvim config to own file

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -160,63 +160,6 @@ return require('packer').startup(function(use)
 
     use {
         'glepnir/dashboard-nvim',
-        config = function()
-            vim.g.dashboard_default_executive = 'telescope'
-            vim.g.dashboard_custom_header = {
-                '', '',
-                '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
-                '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
-                '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
-                '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
-                '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
-                '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
-                ''
-            }
-            vim.g.dashboard_custom_footer = {''}
-            vim.g.dashboard_custom_section = {
-                row1 = {
-                    description = {
-                        '  Recent files                                      '
-                    },
-                    command = 'Telescope oldfiles'
-                },
-                row2 = {
-                    description = {
-                        '  Git file                                <leader>gf'
-                    },
-                    command = 'norm ,gf'
-                },
-                row3 = {
-                    description = {
-                        '  Find file                               <leader>ff'
-                    },
-                    command = 'norm ,ff'
-                },
-                row4 = {
-                    description = {
-                        '  New file                                          '
-                    },
-                    command = 'enew'
-                },
-                row5 = {
-                    description = {
-                        '  File browser                            <leader>fb'
-                    },
-                    command = 'norm ,fb'
-                },
-                row6 = {
-                    description = {
-                        '  Find word                               <leader>fg'
-                    },
-                    command = 'norm ,fg'
-                },
-                row7 = {
-                    description = {
-                        '  Jump to bookmarks                                 '
-                    },
-                    command = 'Telescope marks'
-                }
-            }
-        end
+        config = [[require("tap.plugins.dashboard-nvim")]]
     }
 end)
