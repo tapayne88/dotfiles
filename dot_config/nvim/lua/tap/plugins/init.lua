7936c5eafe7342de1dc107333f9ef9d3c14d945f
Tom Payne <tom@tompayne.dev>
2021-07-15 17:13:24 +0100

    Update vim-rzip function handle duplicate buffer names better - doesn't fix all cases

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -46,24 +46,26 @@ return require('packer').startup(function(use)
                 return substitute(a:uri, '%\([a-fA-F0-9][a-fA-F0-9]\)', '\=nr2char("0x" . submatch(1))', "g")
             endfunction
 
+            function! ClearDuplicateBuffers(uri)
+                if ParseURI(a:uri) !=# a:uri
+                    if bufexists(ParseURI(a:uri))
+                        sil! exe "bwipeout! " . fnameescape(ParseURI(a:uri))
+                    endif
+                    exe "keepalt file " . fnameescape(ParseURI(a:uri))
+                    if bufexists(a:uri)
+                        exe "bwipeout! " . fnameescape(a:uri)
+                    endif
+                endif
+            endfunction
+
             function! RzipOverride()
                 " Disable vim-rzip's autocommands
-                autocmd! zip BufReadCmd zipfile:*,zipfile:*/*
+                autocmd! zip BufReadCmd   zipfile:*,zipfile:*/*
                 exe "au! zip BufReadCmd ".g:zipPlugin_ext
 
-                " First ensure file is named correctly, we don't want % characters
-                autocmd zip BufReadCmd zipfile:*           exe "keepalt file " . fnameescape(ParseURI(expand('<amatch>')))
-                " Re-register vim-rzip's read function with the parsed URI
-                autocmd zip BufReadCmd zipfile:*           call rzip#Read(ParseURI(expand('<amatch>')), 1)
-                " Re-register vim-rzip's browse function for all supported extensions
-                exe "au zip BufReadCmd ".g:zipPlugin_ext." call rzip#Browse(ParseURI(expand('<amatch>')))"
-
-                " Extra unix handling, same as above
-                if has("unix")
-                    autocmd zip BufReadCmd zipfile:*/*  exe "keepalt file " . fnameescape(ParseURI(expand('<amatch>')))
-                    autocmd zip BufReadCmd zipfile:*/*  call rzip#Read(ParseURI(expand('<amatch>')), 1)
-                endif
-
+                autocmd zip BufReadCmd   zipfile:*,zipfile:*/* call ClearDuplicateBuffers(expand("<amatch>"))
+                autocmd zip BufReadCmd   zipfile:*,zipfile:*/* call rzip#Read(ParseURI(expand("<amatch>")), 1)
+                exe "au zip BufReadCmd ".g:zipPlugin_ext."     call rzip#Browse(ParseURI(expand('<amatch>')))"
             endfunction
 
             autocmd VimEnter * call RzipOverride()
