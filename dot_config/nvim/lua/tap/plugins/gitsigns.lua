dde03ce19c6edae209ab2bd4c72f3e94d00ea33f
Tom Payne <tom@tompayne.dev>
2021-08-05 17:54:15 +0100

    Copied gitsign default keymaps and updated to avoid conflicts with quickfix list maps

--- dot_config/nvim/lua/tap/plugins/gitsigns.lua
+++ dot_config/nvim/lua/tap/plugins/gitsigns.lua
@@ -3,7 +3,28 @@ local highlight = require('tap.utils').highlight
 local color = require("tap.utils").color
 local augroup = require("tap.utils").augroup
 
-require('gitsigns').setup()
+require('gitsigns').setup {
+    keymaps = {
+        -- Default keymap options
+        noremap = true,
+
+        ['n [h'] = '<cmd>lua require"gitsigns.actions".prev_hunk()<CR>',
+        ['n ]h'] = '<cmd>lua require"gitsigns.actions".next_hunk()<CR>',
+
+        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+
+        -- Text objects
+        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+    }
+}
 
 nnoremap("gh", "<cmd>lua require('gitsigns').next_hunk()<CR>")
 nnoremap("gH", "<cmd>lua require('gitsigns').prev_hunk()<CR>")
