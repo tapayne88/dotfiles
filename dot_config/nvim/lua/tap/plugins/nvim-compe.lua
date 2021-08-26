2cd5fcf7e2df87897855b55c71365ac9cd9b437e
Tom Payne <tom@tompayne.dev>
2021-08-26 16:27:56 +0100

    Refined nvim-compe config in line with VapourNvim project

--- dot_config/nvim/lua/tap/plugins/nvim-compe.lua
+++ dot_config/nvim/lua/tap/plugins/nvim-compe.lua
@@ -20,6 +20,19 @@ inoremap("<C-k>", "v:lua.completion_nvim.smart_pumvisible('<C-p>', '<C-k>')",
          {expr = true})
 
 require'compe'.setup {
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = 'enable',
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
+
     source = {
         path = {kind = "   (Path)"},
         buffer = {kind = "   (Buffer)"},
@@ -31,7 +44,7 @@ require'compe'.setup {
         tags = false,
         -- snippets_nvim = {kind = "  "},
         -- ultisnips = {kind = "  "},
-        -- treesitter = {kind = "  "},
+        treesitter = {kind = "  "},
         emoji = {kind = " ﲃ  (Emoji)", filetypes = {"markdown", "text"}}
     }
 }
