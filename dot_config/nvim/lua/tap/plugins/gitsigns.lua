86e657d39b16a1876990d02f49f84418b75dd084
Tom Payne <tom@tompayne.dev>
2021-08-27 17:25:00 +0100

    Removed custom highlights for gitsigns, not needed

--- dot_config/nvim/lua/tap/plugins/gitsigns.lua
+++ dot_config/nvim/lua/tap/plugins/gitsigns.lua
@@ -1,7 +1,4 @@
 local nnoremap = require('tap.utils').nnoremap
-local highlight = require('tap.utils').highlight
-local color = require("tap.utils").color
-local augroup = require("tap.utils").augroup
 
 require('gitsigns').setup {
     keymaps = {
@@ -28,19 +25,3 @@ require('gitsigns').setup {
 
 nnoremap("gh", "<cmd>lua require('gitsigns').next_hunk()<CR>")
 nnoremap("gH", "<cmd>lua require('gitsigns').prev_hunk()<CR>")
-
-local function apply_user_highlights()
-    highlight("DiffAdd", {guibg = "NONE", guifg = color("green")})
-    highlight("DiffChange", {guibg = "NONE", guifg = color("yellow")})
-    highlight("DiffDelete", {guibg = "NONE", guifg = color("red")})
-end
-
-augroup("GitSignsHighlights", {
-    {
-        events = {"VimEnter", "ColorScheme"},
-        targets = {"*"},
-        command = apply_user_highlights
-    }
-})
-
-apply_user_highlights()
