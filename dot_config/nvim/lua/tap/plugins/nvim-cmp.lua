f764ef8be0b750312556dff4fb306bfdf8db4e39
Tom Payne <tom@tompayne.dev>
2021-10-15 15:48:56 +0100

    Updated nvim-cmp to use built-in key mapping config due to breaking change, was able to remove hand rolled functions so was a nice change

--- dot_config/nvim/lua/tap/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/tap/plugins/nvim-cmp.lua
@@ -1,7 +1,5 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
-local inoremap = require('tap.utils').inoremap
-local termcodes = require('tap.utils').termcodes
 
 -- Avoid showing message extra message when using completion
 vim.opt.shortmess:append("c")
@@ -9,25 +7,18 @@ vim.opt.shortmess:append("c")
 -- Set completeopt to have a better completion experience
 vim.opt.completeopt = {"menuone", "noselect"}
 
-_G.completion_nvim = {}
-
-function _G.completion_nvim.smart_pumvisible(vis_seq, not_vis_seq)
-    return vim.fn.pumvisible() == 1 and termcodes(vis_seq) or
-               termcodes(not_vis_seq)
-end
-
-inoremap("<C-j>", "v:lua.completion_nvim.smart_pumvisible('<C-n>', '<C-j>')",
-         {expr = true})
-inoremap("<C-k>", "v:lua.completion_nvim.smart_pumvisible('<C-p>', '<C-k>')",
-         {expr = true})
-
 cmp.setup {
     mapping = {
         ['<C-f>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({select = true})
+        ['<C-j>'] = cmp.mapping.select_next_item({
+            behavior = cmp.SelectBehavior.Insert
+        }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({
+            behavior = cmp.SelectBehavior.Insert
+        })
     },
 
     snippet = {
