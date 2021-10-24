b4f1d8558168a9bcd3e8a109d3fa8cdce4736443
Tom Payne <tom@tompayne.dev>
2021-10-24 19:33:03 +0100

    Basic crostini version working with flakes

--- dot_config/nixpkgs/flake.nix
+++ dot_config/nixpkgs/flake.nix
@@ -13,36 +13,49 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-21.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, ... }@inputs:
     let
+      overlay-unstable = final: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          system = prev.system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      };
       overlays = [
+        overlay-unstable
         inputs.neovim-nightly-overlay.overlay
       ];
     in
+    # legacyPackages attribute for declarative channels (used by compat/default.nix)
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+    {
+      legacyPackages = inputs.nixpkgs.legacyPackages.${system};
+    }
+    ) //
     {
       homeConfigurations = {
-        crostini = inputs.home-manager.lib.homeManagerConfiguration {
+        tapayne88 = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-linux";
+          homeDirectory = "/home/tapayne88";
+          username = "tapayne88";
           configuration = { pkgs, ... }:
             {
               nixpkgs.overlays = overlays;
+              nixpkgs.config = {
+                allowUnfree = true;
+              };
 
               # Let Home Manager install and manage itself.
               programs.home-manager.enable = true;
-
-              # This value determines the Home Manager release that your
-              # configuration is compatible with. This helps avoid breakage
-              # when a new Home Manager release introduces backwards
-              # incompatible changes.
-              #
-              # You can update Home Manager without changing this value. See
-              # the Home Manager release notes for a list of state version
-              # changes in each release.
-              home.stateVersion = "21.05";
               
               home.packages = with pkgs; [
                 gcc # neovim - building nvim-treesitter parsers
@@ -102,11 +115,9 @@
                 package = pkgs.neovim-nightly;
               };
             };
-          system = "x86_64-linux";
-          homeDirectory = "/home/tapayne88";
-          username = "tapayne88";
         };
     };
-    crostini = self.homeConfigurations.crostini.activationPackage;
+    cfg = self.homeConfigurations.tapayne88.activationPackage;
+    defaultPackage.x86_64-linux = self.cfg;
   };
 }
