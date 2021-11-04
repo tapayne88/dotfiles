c2949f2ac5a6681efc7f77eec304b2c792e5d3d3
Tom Payne <tom@tompayne.dev>
2021-11-04 11:32:08 +0000

    WIP hit issues with nixgl

--- dot_config/nixpkgs/flake.nix
+++ dot_config/nixpkgs/flake.nix
@@ -30,8 +30,14 @@
           };
         };
       };
+      overlay-nixgl = final: prev: {
+        unstable = import inputs.nixgl {
+          pkgs = inputs.nixpkgs;
+        };
+      };
       overlays = [
         overlay-unstable
+        overlay-nixgl
         inputs.neovim-nightly-overlay.overlay
       ];
     in
@@ -54,66 +60,9 @@
                 allowUnfree = true;
               };
 
-              # Let Home Manager install and manage itself.
-              programs.home-manager.enable = true;
-              
-              home.packages = with pkgs; [
-                gcc # neovim - building nvim-treesitter parsers
-                nq  # linux queue utility
-
-                antigen                       # zsh plugin manager
-                asciinema                     # asciinema.org recorder
-                awscli2                       # cli for working with aws
-                bat                           # colourised cat
-                chezmoi                       # dotfile manager
-                ctop                          # container top process tool
-                curl                          # curl...
-                fpp                           # facebook picker
-                fzf                           # fuzzy-finder
-                gawk                          # gnu awk
-                gitAndTools.diff-so-fancy     # fancy git diffs
-                git                           # git...
-                gnupg                         # gnu pretty good privacy
-                gti                           # alias for git, with a gti
-                htop                          # pretty top
-                inetutils                     # network utils, like traceroute, etc.
-                jq                            # json cli processor
-                kubectx                       # nicer interaction with k8s cli
-                mosh                          # better ssh
-                ncdu                          # disk usage tool
-                netcat                        # netcat implementaion
-                nodejs                        # nodejs...
-                openssh                       # ssh
-                python3                       # python38
-                python38Packages.pip          # pip for python38
-                ripgrep                       # rg searching
-                silver-searcher               # ag searching
-                neofetch                      # pretty print of system info
-                ruby                          # always useful to have ruby
-                tmuxp                         # declaritive tmux session launcher
-                tree                          # print directory structure
-                up                            # utlimate plumber - unix pipes tool
-                unixtools.watch               # run command periodically
-                vim                           # vim
-                watchman                      # watch files and run command on change
-                wget                          # fetch things from urls
-                yarn                          # JS package manager
-                zsh                           # shell
-
-                # Unstable Packages
-                unstable._1password   # 1password version is quite old - unstable is much newer
-                unstable.tmux         # terminal multiplexer
+              imports = [
+                ./home.nix
               ];
-
-
-              programs.neovim = {
-                enable = true;
-                viAlias = true;
-                withNodeJs = true;
-
-                # Neovim nightly
-                package = pkgs.neovim-nightly;
-              };
             };
         };
     };
