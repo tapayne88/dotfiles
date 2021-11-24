67e02f8e4a5b05e7394bba4fb3f23ad114bc32b4
Tom Payne <tom@tompayne.dev>
2021-11-24 23:10:58 +0000

    Added jsonls to lsp setup and updated config

--- dot_config/nvim/lua/tap/lsp/servers/jsonls.lua
+++ dot_config/nvim/lua/tap/lsp/servers/jsonls.lua
@@ -2,12 +2,12 @@ local lsp_utils = require('tap.lsp.utils')
 
 local module = {}
 
-function module.setup()
-    lsp_utils.lspconfig_server_setup("json", {
+function module.setup(lsp_server)
+    lsp_server:setup(lsp_utils.get_default_config({
         on_attach = lsp_utils.on_attach,
         init_options = {provideFormatter = false},
         settings = {json = {schemas = require('schemastore').json.schemas()}}
-    })
+    }))
 end
 
 return module
--- dot_config/nvim/lua/tap/plugins/lspconfig.lua
+++ dot_config/nvim/lua/tap/plugins/lspconfig.lua
@@ -12,7 +12,7 @@ utils.command({
     function() print(vim.inspect(require'lspinstall'.installed_servers())) end
 })
 
-local servers = {"diagnosticls", "eslint"}
+local servers = {"diagnosticls", "eslint", "jsonls"}
 
 local function init_servers()
     for _, name in pairs(servers) do
