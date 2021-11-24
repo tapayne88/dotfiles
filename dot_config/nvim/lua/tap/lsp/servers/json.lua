696588382046b36505dc2a39e9eb6fd3f3500e27
Tom Payne <tom@tompayne.dev>
2021-11-24 14:07:23 +0000

    Tweaked jsonls config to be come concise
    Moved formatter disabling to config rather than programmatic interface,
    easier to grok

--- dot_config/nvim/lua/tap/lsp/servers/json.lua
+++ dot_config/nvim/lua/tap/lsp/servers/json.lua
@@ -4,11 +4,8 @@ local module = {}
 
 function module.setup()
     lsp_utils.lspconfig_server_setup("json", {
-        on_attach = function(client, bufnr)
-            -- Prevent document_formatting, that's diagnosticls' job
-            client.resolved_capabilities.document_formatting = false
-            lsp_utils.on_attach(client, bufnr)
-        end,
+        on_attach = lsp_utils.on_attach,
+        init_options = {provideFormatter = false},
         settings = {json = {schemas = require('schemastore').json.schemas()}}
     })
 end
