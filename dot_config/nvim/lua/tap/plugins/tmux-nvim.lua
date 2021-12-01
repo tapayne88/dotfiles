2307a49aeeb35e0eb110a22cc918ef92fa30b5ed
Tom Payne <tom@tompayne.dev>
2021-12-01 10:03:30 +0000

    Updated tmux-nvim config to work better with netrw
    Had to rebind navigation keys to override some netrw defaults

--- dot_config/nvim/lua/tap/plugins/tmux-nvim.lua
+++ dot_config/nvim/lua/tap/plugins/tmux-nvim.lua
@@ -1,3 +1,6 @@
+local augroup = require("tap.utils").augroup
+local nnoremap = require("tap.utils").nnoremap
+
 require("tmux").setup({
     navigation = {
         -- cycles to opposite pane while navigating into the border
@@ -6,5 +9,18 @@ require("tmux").setup({
         enable_default_keybindings = true,
         -- prevents unzoom tmux when navigating beyond vim border
         persist_zoom = true
+    }
+})
+
+augroup("TmuxNvimNetrw", {
+    {
+        events = {"Filetype"},
+        targets = {"netrw"},
+        command = function()
+            nnoremap("<C-h>", require'tmux'.move_left, {bufnr = "<buffer>"})
+            nnoremap("<C-j>", require'tmux'.move_bottom, {bufnr = "<buffer>"})
+            nnoremap("<C-k>", require'tmux'.move_top, {bufnr = "<buffer>"})
+            nnoremap("<C-l>", require'tmux'.move_right, {bufnr = "<buffer>"})
+        end
     }
 })
