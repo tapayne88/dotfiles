0c707c3fc945fa6cf0980a95a3f8c80976b95e05
Tom Payne <tom@tompayne.dev>
2021-12-07 19:17:03 +0000

    Reverted changes to minimal.lua
    Ready for the next issue!

--- dot_config/nvim/minimal.lua
+++ dot_config/nvim/minimal.lua
@@ -11,7 +11,7 @@ local install_path = package_root .. '/packer/start/packer.nvim'
 local function load_plugins()
     require('packer').startup {
         {
-            'wbthomason/packer.nvim', 'lbrayner/vim-rzip'
+            'wbthomason/packer.nvim'
             -- ADD PLUGINS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
         },
         config = {
@@ -24,50 +24,6 @@ end
 
 _G.load_config = function()
     -- ADD INIT.LUA SETTINGS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
-    vim.cmd [[
-" Decode URI encoded characters
-function! DecodeURI(uri)
-    return substitute(a:uri, '%\([a-fA-F0-9][a-fA-F0-9]\)', '\=nr2char("0x" . submatch(1))', "g")
-endfunction
-
-" Attempt to clear non-focused buffers with matching name
-function! ClearDuplicateBuffers(uri)
-echo 'a:uri: '. a:uri
-echo 'expand("<amatch>"): '. expand("<amatch>")
-echo 'expand("<afile>"): '. expand("<afile>")
-echo 'DecodeURI(a:uri): '. DecodeURI(a:uri)
-
-    " if our filename has URI encoded characters
-    if DecodeURI(a:uri) !=# a:uri
-        " wipeout buffer with URI decoded name - can print error if buffer in focus
-        sil! exe "bwipeout " . fnameescape(DecodeURI(a:uri))
-        " change the name of the current buffer to the URI decoded name
-        exe "keepalt file " . fnameescape(DecodeURI(a:uri))
-        " ensure we don't have any open buffer matching non-URI decoded name
-        sil! exe "bwipeout " . fnameescape(a:uri)
-    endif
-endfunction
-
-function! RzipOverride()
-echo "overriding"
-    " Disable vim-rzip's autocommands
-    autocmd! zip BufReadCmd   zipfile:*,zipfile:*/*
-    exe "au! zip BufReadCmd ".g:zipPlugin_ext
-
-    " order is important here, setup name of new buffer correctly then fallback to vim-rzip's handling
-    autocmd zip BufReadCmd   zipfile:*  call ClearDuplicateBuffers(expand("<afile>"))
-    autocmd zip BufReadCmd   zipfile:*  call rzip#Read(DecodeURI(expand("<afile>")), 1)
-
-    if has("unix")
-        autocmd zip BufReadCmd   zipfile:*/*  call ClearDuplicateBuffers(expand("<afile>"))
-        autocmd zip BufReadCmd   zipfile:*/*  call rzip#Read(DecodeURI(expand("<afile>")), 1)
-    endif
-
-    exe "au zip BufReadCmd ".g:zipPlugin_ext."  call rzip#Browse(DecodeURI(expand('<afile>')))"
-endfunction
-
-call RzipOverride()
-]]
 end
 
 if vim.fn.isdirectory(install_path) == 0 then
