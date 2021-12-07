35b459221ad1e16603159f41f2a2e178f192e081
Tom Payne <tom@tompayne.dev>
2021-12-07 18:06:18 +0000

    Fix for vim-rzip yarn pnp archives
    For some reason this has stopped working. It looks like
    `expand('<amatch>')` now includes the current working directory which I
    assume is because the begin 'zipfile:' (no preceding '/')

--- dot_config/nvim/lua/tap/plugins/vim-rzip.lua
+++ dot_config/nvim/lua/tap/plugins/vim-rzip.lua
@@ -5,16 +5,23 @@ function! DecodeURI(uri)
 endfunction
 
 " Attempt to clear non-focused buffers with matching name
-function! ClearDuplicateBuffers(uri)
+function! ClearDuplicateBuffers()
+    let uri = bufname()
+
     " if our filename has URI encoded characters
-    if DecodeURI(a:uri) !=# a:uri
+    if DecodeURI(uri) !=# uri
         " wipeout buffer with URI decoded name - can print error if buffer in focus
-        sil! exe "bwipeout " . fnameescape(DecodeURI(a:uri))
+        sil! exe "bwipeout " . fnameescape(DecodeURI(uri))
         " change the name of the current buffer to the URI decoded name
-        exe "keepalt file " . fnameescape(DecodeURI(a:uri))
+        exe "keepalt file " . fnameescape(DecodeURI(uri))
         " ensure we don't have any open buffer matching non-URI decoded name
-        sil! exe "bwipeout " . fnameescape(a:uri)
+        sil! exe "bwipeout " . fnameescape(uri)
     endif
+endfunction
+
+function! RzipRead()
+    let uri = bufname()
+    call rzip#Read(DecodeURI(uri), 1)
 endfunction
 
 function! RzipOverride()
@@ -23,12 +30,12 @@ function! RzipOverride()
     exe "au! zip BufReadCmd ".g:zipPlugin_ext
 
     " order is important here, setup name of new buffer correctly then fallback to vim-rzip's handling
-    autocmd zip BufReadCmd   zipfile:*  call ClearDuplicateBuffers(expand("<amatch>"))
-    autocmd zip BufReadCmd   zipfile:*  call rzip#Read(DecodeURI(expand("<amatch>")), 1)
+    autocmd zip BufReadCmd   zipfile:*  call ClearDuplicateBuffers()
+    autocmd zip BufReadCmd   zipfile:*  call RzipRead()
 
     if has("unix")
-        autocmd zip BufReadCmd   zipfile:*/*  call ClearDuplicateBuffers(expand("<amatch>"))
-        autocmd zip BufReadCmd   zipfile:*/*  call rzip#Read(DecodeURI(expand("<amatch>")), 1)
+        autocmd zip BufReadCmd   zipfile:*/*  call ClearDuplicateBuffers()
+        autocmd zip BufReadCmd   zipfile:*/*  call RzipRead()
     endif
 
     exe "au zip BufReadCmd ".g:zipPlugin_ext."  call rzip#Browse(DecodeURI(expand('<amatch>')))"
