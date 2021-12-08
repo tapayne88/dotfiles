dda93429ddffc5a95e1237b8a349cf091f957191
Tom Payne <tom@tompayne.dev>
2021-12-08 23:57:02 +0000

    Updated flake inputs to use release 21.11
    From 21.05, also updated home-manager variables

--- dot_config/nixpkgs/flake.nix
+++ dot_config/nixpkgs/flake.nix
@@ -2,7 +2,7 @@
   description = "Home-manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
@@ -13,7 +13,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.05";
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -45,7 +45,7 @@
           system = "x86_64-linux";
           homeDirectory = "/home/tapayne88";
           username = "tapayne88";
-          stateVersion = "21.05";
+          stateVersion = "21.11";
           configuration = { pkgs, ... }:
             {
               nixpkgs.overlays = overlays;
@@ -64,7 +64,7 @@
           system = "x86_64-darwin";
           homeDirectory = "/Users/thomas.payne";
           username = "thomas.payne";
-          stateVersion = "21.05";
+          stateVersion = "21.11";
           configuration = { pkgs, ... }:
             {
               nixpkgs.overlays = overlays;
