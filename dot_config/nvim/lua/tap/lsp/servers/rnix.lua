10ceb1cf628f7eaaa6b31154aafde9fa790ab847
Tom Payne <tom@tompayne.dev>
2021-12-09 16:50:49 +0000

    Setup rnix with all default config values

--- dot_config/nvim/lua/tap/lsp/servers/rnix.lua
+++ dot_config/nvim/lua/tap/lsp/servers/rnix.lua
@@ -3,7 +3,7 @@ local lsp_utils = require "tap.lsp.utils"
 local module = {}
 
 function module.setup()
-    require'lspconfig'.rnix.setup {on_attach = lsp_utils.on_attach}
+    require'lspconfig'.rnix.setup {lsp_utils.merge_with_default_config()}
 end
 
 return module
