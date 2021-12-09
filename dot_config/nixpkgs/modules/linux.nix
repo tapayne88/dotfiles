cfb4f7cb4e816e0f567eddaa948d0ebd0a153c73
Tom Payne <tom@tompayne.dev>
2021-12-09 09:51:12 +0000

    Removed xdg.mine home-manager property
    Can't remember why this was necessary and its removal doesn't seem to
    break anything now so going to try live without it

--- dot_config/nixpkgs/modules/linux.nix
+++ dot_config/nixpkgs/modules/linux.nix
@@ -1,8 +1,5 @@
 { pkgs, ... }:
 {
-  # Fix I/O error with XML write - root cause unknown
-  xdg.mime.enable = false;
-
   home.packages = with pkgs; [
     gcc # neovim - building nvim-treesitter parsers
     nq  # linux queue utility
