9d8f085654baa2c000777e8c5769fdb4daabcf51
Tom Payne <tom@tompayne.dev>
2021-12-12 17:42:19 +0000

    Fixed rnix config not setuping server
    Error in syntax when calling .setup

--- dot_config/nvim/lua/tap/lsp/servers/rnix.lua
+++ dot_config/nvim/lua/tap/lsp/servers/rnix.lua
@@ -3,7 +3,7 @@ local lsp_utils = require "tap.lsp.utils"
 local module = {}
 
 function module.setup()
-    require'lspconfig'.rnix.setup {lsp_utils.merge_with_default_config()}
+    require'lspconfig'.rnix.setup(lsp_utils.merge_with_default_config())
 end
 
 return module
