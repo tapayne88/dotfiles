c2062f2c8fd17dced0fe566223d546681783d496
Tom Payne <tom@tompayne.dev>
2021-12-14 11:07:11 +0000

    Added sh.chezmoitmpl to bashls lsp

--- dot_config/nvim/lua/tap/lsp/servers/bashls.lua
+++ dot_config/nvim/lua/tap/lsp/servers/bashls.lua
@@ -3,7 +3,9 @@ local lsp_utils = require "tap.lsp.utils"
 local module = {}
 
 function module.setup(lsp_server)
-    lsp_server:setup(lsp_utils.merge_with_default_config())
+    lsp_server:setup(lsp_utils.merge_with_default_config {
+        filetypes = {'sh', 'sh.chezmoitmpl'}
+    })
 end
 
 return module
