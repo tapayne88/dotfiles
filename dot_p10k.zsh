c8608f9339dad6460d5e4bad2f00eafc1e2c9ccd
Tom Payne <tom@tompayne.dev>
2021-12-20 14:49:28 +0000

    Added worktree information to git prompt

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -416,6 +416,10 @@
       res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"
     fi
 
+    if [[ $(git worktree list 2> /dev/null | wc -l) -gt 1 ]]; then
+      res+="%8F($(basename $(git rev-parse --show-toplevel)))"
+    fi
+
     # Display "wip" if the latest commit's summary contains "wip" or "WIP".
     if [[ $VCS_STATUS_COMMIT_SUMMARY == (|*[^[:alnum:]])(wip|WIP)(|[^[:alnum:]]*) ]]; then
       res+=" ${modified}wip"
