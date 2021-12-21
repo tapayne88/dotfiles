4c36fea540171610b2f3e242d8dc87c9a49345dc
Tom Payne <tom@tompayne.dev>
2021-12-21 16:40:28 +0000

    Tried to make anchored dir stand out in prompt

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -208,7 +208,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=3
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=11
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
