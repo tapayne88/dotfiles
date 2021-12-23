31c4019a101a4d5b58046c26f2a4e332532ef636
Tom Payne <tom@tompayne.dev>
2021-12-23 14:41:09 +0000

    Tweaked script to use bat pager

--- dot_local/bin/executable_tmux-cht.sh
+++ dot_local/bin/executable_tmux-cht.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+
+languages="$XDG_CONFIG_HOME/tmux-chtsh/languages"
+commands="$XDG_CONFIG_HOME/tmux-chtsh/commands"
+
+selected=$(cat "$languages" "$commands" | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-read -p "Enter Query: " query
+read -r -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if grep -qs "$selected" "$languages"; then
+    query=$(echo "$query" | tr ' ' '+')
+    if bat --list-languages | grep -i "\b$selected\b"; then
+        syntax="--language $selected"
+    else
+        syntax=""
+    fi
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl -s cht.sh/$selected/$query\?T | bat --paging always --style plain $syntax"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "echo \"curl cht.sh/$selected~$query/\" & curl -s cht.sh/$selected~$query\?T | bat --paging always --style plain --language bash"
 fi
-
