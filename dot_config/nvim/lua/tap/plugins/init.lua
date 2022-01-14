93e3ad1df72a4bba129a238f8ba8c323d2865abb
Tom Payne <tom@tompayne.dev>
2022-01-14 20:31:33 +0000

    Added mappy as a dependency, nesting which-key

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -183,14 +183,17 @@ return require('packer').startup(function(use)
     }
 
     use {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
+        "shift-d/mappy.nvim",
+        requires = {
+            "folke/which-key.nvim",
+            config = function()
+                require("which-key").setup {
+                    -- your configuration comes here
+                    -- or leave it empty to use the default settings
+                    -- refer to the configuration section below
+                }
+            end
+        }
     }
 
     use {
