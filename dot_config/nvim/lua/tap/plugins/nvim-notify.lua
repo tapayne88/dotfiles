ead9b5da226e5879e3c6c1adff2224423bffcb86
Tom Payne <tom@tompayne.dev>
2022-01-16 18:00:46 +0000

    Updated notify info color to be green

--- dot_config/nvim/lua/tap/plugins/nvim-notify.lua
+++ dot_config/nvim/lua/tap/plugins/nvim-notify.lua
@@ -21,19 +21,19 @@ local function apply_user_highlights()
     -- LuaFormatter off
     highlight("NotifyERRORBorder", {guifg = color({dark = "nord11_gui", light = "red"})})
     highlight("NotifyWARNBorder", {guifg = color({dark = "nord13_gui", light = "yellow"})})
-    highlight("NotifyINFOBorder", {guifg = color({dark = "nord4_gui", light = "fg"})})
+    highlight("NotifyINFOBorder", {guifg = color({dark = "nord14_gui", light = "green"})})
     highlight("NotifyDEBUGBorder", {guifg = color({dark = "nord10_gui", light = "blue2"})})
     highlight("NotifyTRACEBorder", {guifg = color({dark="nord15_gui", light="purple"})})
 
     highlight("NotifyERRORIcon", {guifg = color({dark = "nord11_gui", light = "red"})})
     highlight("NotifyWARNIcon", {guifg = color({dark = "nord13_gui", light = "yellow"})})
-    highlight("NotifyINFOIcon", {guifg = color({dark = "nord4_gui", light = "fg"})})
+    highlight("NotifyINFOIcon", {guifg = color({dark = "nord14_gui", light = "green"})})
     highlight("NotifyDEBUGIcon", {guifg = color({dark = "nord10_gui", light = "blue2"})})
     highlight("NotifyTRACEIcon", {guifg = color({dark="nord15_gui", light="purple"})})
 
     highlight("NotifyERRORTitle", {guifg = color({dark = "nord11_gui", light = "red"})})
     highlight("NotifyWARNTitle", {guifg = color({dark = "nord13_gui", light = "yellow"})})
-    highlight("NotifyINFOTitle", {guifg = color({dark = "nord4_gui", light = "fg"})})
+    highlight("NotifyINFOTitle", {guifg = color({dark = "nord14_gui", light = "green"})})
     highlight("NotifyDEBUGTitle", {guifg = color({dark = "nord10_gui", light = "blue2"})})
     highlight("NotifyTRACETitle", {guifg = color({dark="nord15_gui", light="purple"})})
     -- LuaFormatter on
