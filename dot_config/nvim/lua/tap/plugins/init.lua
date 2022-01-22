662ae7fd2465677c9bcb2d8888173f95791b77be
Tom Payne <tom@tompayne.dev>
2022-01-22 13:53:04 +0000

    Added cmp-path and nvim-lua as sources to nvim-cmp

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -164,8 +164,8 @@ return require('packer').startup(function(use)
         'hrsh7th/nvim-cmp',
         config = [[require("tap.plugins.nvim-cmp")]],
         requires = {
-            'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer',
-            'onsails/lspkind-nvim', 'L3MON4D3/LuaSnip',
+            'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer', 'onsails/lspkind-nvim', 'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip'
         }
     }
--- dot_config/nvim/lua/tap/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/tap/plugins/nvim-cmp.lua
@@ -25,7 +25,10 @@ cmp.setup {
         expand = function(args) require('luasnip').lsp_expand(args.body) end
     },
 
-    sources = {{name = 'nvim_lsp'}, {name = 'buffer'}, {name = 'luasnip'}},
+    sources = {
+        {name = 'nvim_lsp'}, {name = "nvim_lua"}, {name = "path"},
+        {name = 'buffer'}, {name = 'luasnip'}
+    },
 
     formatting = {format = lspkind.cmp_format()}
 }
