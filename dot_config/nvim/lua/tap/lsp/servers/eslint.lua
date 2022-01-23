8b7f646c9fcd5f213e75451e62d1e850f489e739
Tom Payne <tom@tompayne.dev>
2022-01-23 14:05:13 +0000

    Prevent eslint showing prompt on server error

--- dot_config/nvim/lua/tap/lsp/servers/eslint.lua
+++ dot_config/nvim/lua/tap/lsp/servers/eslint.lua
@@ -4,6 +4,12 @@ local module = {}
 
 function module.setup(lsp_server)
     lsp_server:setup(lsp_utils.merge_with_default_config({
+        handlers = {
+            -- Prevent eslint popup prompt when language server throws an error
+            ['window/showMessageRequest'] = function(_, result)
+                return result
+            end
+        },
         settings = {packageManager = 'yarn'}
     }))
 end
