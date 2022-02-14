bf17c0737a971fa1d08b5d6872d2ca93e4f46783
Tom Payne <tom@tompayne.dev>
2022-02-14 18:18:15 +0000

    Removed make from neovim as breaks work makefile
    Added to linux to just target wsl & crostini

--- dot_config/nixpkgs/modules/linux.nix
+++ dot_config/nixpkgs/modules/linux.nix
@@ -2,6 +2,7 @@
 {
   home.packages = with pkgs; [
     gcc # neovim - building nvim-treesitter parsers
+    gnumake # neovim treesitter
     nq # linux queue utility
     unzip # wsl doesn't include unzip
   ];
