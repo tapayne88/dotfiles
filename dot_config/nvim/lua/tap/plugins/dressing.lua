cd106477ec8a23222e08f56b143dc2c7c36ae67c
Tom Payne <tom@tompayne.dev>
2022-03-03 15:29:05 +0000

    Removed deprecated properties from dressing config
    Also cleaned up config for unused view types, only use telescope

--- dot_config/nvim/lua/tap/plugins/dressing.lua
+++ dot_config/nvim/lua/tap/plugins/dressing.lua
@@ -9,8 +9,6 @@ require('dressing').setup({
         -- These are passed to nvim_open_win
         anchor = "SW",
         relative = "cursor",
-        row = 0,
-        col = 0,
         border = "rounded",
 
         -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
@@ -28,45 +26,12 @@ require('dressing').setup({
     },
     select = {
         -- Priority list of preferred vim.select implementations
-        backend = {"telescope", "builtin", "nui"},
+        backend = {"telescope"},
 
         -- Options for telescope selector
         telescope = {
             -- can be 'dropdown', 'cursor', or 'ivy'
             theme = "dropdown"
-        },
-
-        -- Options for nui Menu
-        nui = {
-            position = "50%",
-            size = nil,
-            relative = "editor",
-            border = {style = "rounded"},
-            max_width = 80,
-            max_height = 40
-        },
-
-        -- Options for built-in selector
-        builtin = {
-            -- These are passed to nvim_open_win
-            anchor = "NW",
-            relative = "cursor",
-            row = 0,
-            col = 0,
-            border = "rounded",
-
-            -- Window transparency (0-100)
-            winblend = 0,
-            -- Change default highlight groups (see :help winhl)
-            winhighlight = "",
-
-            -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-            width = nil,
-            max_width = 0.8,
-            min_width = 40,
-            height = nil,
-            max_height = 0.9,
-            min_height = 10
         },
 
         -- Used to override format_item. See :help dressing-format
