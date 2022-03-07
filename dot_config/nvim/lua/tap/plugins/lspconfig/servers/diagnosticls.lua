166a8a63beea418010ec38964e5842626aa37a35
Tom Payne <tom@tompayne.dev>
2022-03-07 13:59:31 +0000

    Swapped to prettierd to detect prettier version
    The prettier version should be the nearest prettier executable to the
    file being edited, this is tricky to detect with the lsp config &
    diagnosticls so using prettierd to do it

--- dot_config/nvim/lua/tap/plugins/lspconfig/servers/diagnosticls.lua
+++ dot_config/nvim/lua/tap/plugins/lspconfig/servers/diagnosticls.lua
@@ -2,7 +2,6 @@ local servers = require "nvim-lsp-installer.servers"
 local installers = require "nvim-lsp-installer.installers"
 local npm = require "nvim-lsp-installer.installers.npm"
 local shell = require "nvim-lsp-installer.installers.shell"
-local a = require "plenary.async"
 local utils = require "tap.utils"
 local lsp_utils = require "tap.utils.lsp"
 
@@ -27,7 +26,7 @@ local module = {}
 function module.patch_install()
     lsp_utils.patch_lsp_installer("diagnosticls", installers.pipe {
         npm.packages {
-            "diagnostic-languageserver", "prettier", "markdownlint-cli"
+            "diagnostic-languageserver", "@fsouza/prettierd", "markdownlint-cli"
         }, shell.bash(lua_format)
     })
 end
@@ -60,75 +59,72 @@ local diagnosticls_languages = {
 }
 
 function module.setup(lsp_server)
-    a.run(function()
-        local root_dir = servers.get_server_install_path(lsp_server.name)
-        local prettier_bin = lsp_utils.get_bin_path("prettier")
+    local root_dir = servers.get_server_install_path(lsp_server.name)
 
-        lsp_server:setup(lsp_utils.merge_with_default_config({
-            filetypes = vim.tbl_keys(diagnosticls_languages),
-            init_options = {
-                linters = {
-                    markdownlint = {
-                        command = "markdownlint",
-                        isStderr = true,
-                        debounce = 100,
-                        args = {
-                            "--config",
-                            vim.fn.stdpath("config") .. "/markdownlint.json",
-                            "--stdin"
-                        },
-                        offsetLine = 0,
-                        offsetColumn = 0,
-                        sourceName = "markdownlint",
-                        formatLines = 1,
-                        formatPattern = {
-                            "^.*?:\\s?(\\d+)(:(\\d+)?)?\\s(MD\\d{3}\\/[A-Za-z0-9-/]+)\\s(.*)$",
-                            {line = 1, column = 3, message = {4}}
-                        }
+    lsp_server:setup(lsp_utils.merge_with_default_config({
+        filetypes = vim.tbl_keys(diagnosticls_languages),
+        init_options = {
+            linters = {
+                markdownlint = {
+                    command = "markdownlint",
+                    isStderr = true,
+                    debounce = 100,
+                    args = {
+                        "--config",
+                        vim.fn.stdpath("config") .. "/markdownlint.json",
+                        "--stdin"
                     },
-                    shellcheck = {
-                        command = "shellcheck",
-                        debounce = 100,
-                        args = {"--format", "json", "-"},
-                        sourceName = "shellcheck",
-                        parseJson = {
-                            line = "line",
-                            column = "column",
-                            endLine = "endLine",
-                            endColumn = "endColumn",
-                            message = "${message} [${code}]",
-                            security = "level"
-                        },
-                        securities = {
-                            error = "error",
-                            warning = "warning",
-                            info = "info",
-                            style = "hint"
-                        }
+                    offsetLine = 0,
+                    offsetColumn = 0,
+                    sourceName = "markdownlint",
+                    formatLines = 1,
+                    formatPattern = {
+                        "^.*?:\\s?(\\d+)(:(\\d+)?)?\\s(MD\\d{3}\\/[A-Za-z0-9-/]+)\\s(.*)$",
+                        {line = 1, column = 3, message = {4}}
                     }
                 },
-                filetypes = map_language_to_filetype(diagnosticls_languages,
-                                                     "linters"),
-                formatters = {
-                    prettier = {
-                        command = prettier_bin or "prettier",
-                        args = {"--stdin-filepath", "%filepath"},
-                        rootPatterns = {
-                            "package.json", ".prettierrc", ".prettierrc.json",
-                            ".prettierrc.toml", ".prettierrc.json",
-                            ".prettierrc.yml", ".prettierrc.yaml",
-                            ".prettierrc.json5", ".prettierrc.js",
-                            ".prettierrc.cjs", "prettier.config.js",
-                            "prettier.config.cjs"
-                        }
+                shellcheck = {
+                    command = "shellcheck",
+                    debounce = 100,
+                    args = {"--format", "json", "-"},
+                    sourceName = "shellcheck",
+                    parseJson = {
+                        line = "line",
+                        column = "column",
+                        endLine = "endLine",
+                        endColumn = "endColumn",
+                        message = "${message} [${code}]",
+                        security = "level"
                     },
-                    lua_format = {command = root_dir .. "/lua-format"}
+                    securities = {
+                        error = "error",
+                        warning = "warning",
+                        info = "info",
+                        style = "hint"
+                    }
+                }
+            },
+            filetypes = map_language_to_filetype(diagnosticls_languages,
+                                                 "linters"),
+            formatters = {
+                prettier = {
+                    command = "prettierd",
+                    args = {"%filepath"},
+                    rootPatterns = {
+                        "package.json", ".prettierrc", ".prettierrc.json",
+                        ".prettierrc.toml", ".prettierrc.json",
+                        ".prettierrc.yml", ".prettierrc.yaml",
+                        ".prettierrc.json5", ".prettierrc.js",
+                        ".prettierrc.cjs", "prettier.config.js",
+                        "prettier.config.cjs"
+                    }
                 },
-                formatFiletypes = map_language_to_filetype(
-                    diagnosticls_languages, "formatters")
-            }
-        }))
-    end)
+                lua_format = {command = root_dir .. "/lua-format"}
+            },
+            formatFiletypes = map_language_to_filetype(diagnosticls_languages,
+                                                       "formatters")
+        }
+    }))
 end
 
 return module
