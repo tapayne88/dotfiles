721dbf907b91d2c3a881dabcd65ae65fa187c6d1
Tom Payne <tom@tompayne.dev>
2022-03-25 11:11:37 +0000

    Made neovim respond to term-theme toggle
    First pass of watching term-theme file for changes

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -49,6 +49,7 @@ return require('packer').startup(function(use)
     use 'nvim-lua/plenary.nvim'                         -- Utility function used by plugins and my config
     use 'RRethy/vim-illuminate'                         -- Highlight same words
     use 'nathom/filetype.nvim'                          -- A faster version of filetype.vim
+    use 'rktjmp/fwatch.nvim'                            -- Utility for watching files
     -- LuaFormatter on
 
     -- Chunk cache for neovim modules
