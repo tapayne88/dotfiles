b8ea961f98b853c4528cb7d64c5baf68bbe0768c
Tom Payne <tom@tompayne.dev>
2022-04-09 17:17:45 +0100

    Added multi cursor editing mappings

--- dot_config/nvim/lua/tap/keymap.lua
+++ dot_config/nvim/lua/tap/keymap.lua
@@ -1,6 +1,7 @@
 local nnoremap = require("tap.utils").nnoremap
 local vnoremap = require("tap.utils").vnoremap
 local tnoremap = require("tap.utils").tnoremap
+local xnoremap = require("tap.utils").xnoremap
 
 nnoremap('j', 'gj', {description = "Jump down one wrapped line"})
 nnoremap('k', 'gk', {description = "Jump up one wrapped line"})
@@ -35,3 +36,44 @@ tnoremap('<Esc>', [[<C-\><C-n>]],
          {description = "Escape terminal insert mode with <Esc>"})
 tnoremap('<A-[>', '<Esc>',
          {description = "Alternate mapping to send esc to terminal"})
+
+-- Multiple Cursors
+-- http://www.kevinli.co/posts/2017-01-19-multiple-cursors-in-500-bytes-of-vimscript/
+-- https://github.com/akinsho/dotfiles/blob/45c4c17084d0aa572e52cc177ac5b9d6db1585ae/.config/nvim/plugin/mappings.lua#L298
+
+-- 1. Position the cursor anywhere in the word you wish to change;
+-- 2. Or, visually make a selection;
+-- 3. Hit cn, type the new word, then go back to Normal mode;
+-- 4. Hit `.` n-1 times, where n is the number of replacements.
+nnoremap("cn", "*``cgn", {description = "Multiple cursors"})
+xnoremap("cn", [[g:mc . "``cgn"]],
+         {expr = true, description = "Multiple cursors"})
+
+nnoremap("cN", "*``cgN", {description = "Multiple cursors (backwards)"})
+xnoremap("cN", [[g:mc . "``cgN"]],
+         {expr = true, description = "Multiple cursors (backwards)"})
+
+-- 1. Position the cursor over a word; alternatively, make a selection.
+-- 2. Hit cq to start recording the macro.
+-- 3. Once you are done with the macro, go back to normal mode.
+-- 4. Hit Enter to repeat the macro over search matches.
+nnoremap("cq", [[:lua tap.mappings.setup_mc()<CR>*``qz]],
+         {description = "Multiple cursors: Macros"})
+xnoremap("cq", [[":\<C-u>lua tap.mappings.setup_mc()\<CR>gv" . g:mc . "``qz"]],
+         {expr = true, description = "Multiple cursors: Macros"})
+
+nnoremap("cQ", [[:lua tap.mappings.setup_mc()<CR>#``qz]],
+         {description = "Multiple cursors: Macros (backwards)"})
+xnoremap("cQ",
+         [[":\<C-u>lua tap.mappings.setup_CR()\<CR>gv" . substitute(g:mc, '/', '?', 'g') . "``qz"]],
+         {expr = true, description = "Multiple cursors: Macros (backwards)"})
+
+-- Functions for multiple cursors
+vim.g.mc = vim.api.nvim_replace_termcodes([[y/\V<C-r>=escape(@", '/')<CR><CR>]],
+                                          true, true, true)
+
+function tap.mappings.setup_mc()
+    vim.keymap.set("n", "<Enter>",
+                   [[:nnoremap <lt>Enter> n@z<CR>q:<C-u>let @z=strpart(@z,0,strlen(@z)-1)<CR>n@z]],
+                   {remap = true, silent = true})
+end
