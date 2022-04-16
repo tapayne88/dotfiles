dd5151b7e4540b7c03d571cf5b2f9dd3d0416325
Tom Payne <tom@tompayne.dev>
2022-04-16 11:36:16 +0100

    Updated nightly version to 0.8

--- dot_config/nvim/lua/tap/globals.lua
+++ dot_config/nvim/lua/tap/globals.lua
@@ -13,7 +13,7 @@ function tap._execute(id, ...) tap._store[id](...) end
 
 --- Check if neovim version is nightly
 function tap.neovim_nightly()
-    local nightly = '0.7'
+    local nightly = '0.8'
     local version = vim.version()
     return string.format('%i.%i', version.major, version.minor) == nightly
 end
