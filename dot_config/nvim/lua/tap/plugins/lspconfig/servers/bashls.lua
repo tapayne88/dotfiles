007a8aa8ed7adfb63456bafdfce1c29f1f7ee2b5
Tom Payne <tom@tompayne.dev>
2022-04-30 18:36:53 +0100

    Removed chezmoi template config from bashls

--- dot_config/nvim/lua/tap/plugins/lspconfig/servers/bashls.lua
+++ dot_config/nvim/lua/tap/plugins/lspconfig/servers/bashls.lua
@@ -3,9 +3,7 @@ local lsp_utils = require 'tap.utils.lsp'
 local module = {}
 
 function module.setup()
-  require('lspconfig').bashls.setup(lsp_utils.merge_with_default_config {
-    filetypes = { 'sh', 'sh.chezmoitmpl' },
-  })
+  require('lspconfig').bashls.setup(lsp_utils.merge_with_default_config())
 end
 
 return module
