97ec8f4d164785051e5c6918b10db762e2a048d5
Tom Payne <tom@tompayne.dev>
2022-05-16 11:08:22 +0100

    Reduced nix generation retention to reduce disk usage

--- dot_config/zsh/functions/nix.zsh
+++ dot_config/zsh/functions/nix.zsh
@@ -1,6 +1,6 @@
 function nix-clean() {
   set -x
-  local DEFAULT_MAX_AGE_DAYS="30"
+  local DEFAULT_MAX_AGE_DAYS="7"
 
   home-manager expire-generations "-${1:-$DEFAULT_MAX_AGE_DAYS} days"
   nix-env --delete-generations "${1:-$DEFAULT_MAX_AGE_DAYS}d"
