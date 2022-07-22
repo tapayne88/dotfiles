de777f6d355e38f3508817abc7a000d6afdc18ab
Tom Payne <tom@tompayne.dev>
2022-07-22 15:41:51 +0100

    Add clean prompt to nix-update to prune generations

--- dot_config/zsh/functions/nix.zsh
+++ dot_config/zsh/functions/nix.zsh
@@ -1,7 +1,3 @@
 function nix-clean() {
-  set -x
-  local DEFAULT_MAX_AGE_DAYS="7"
-
-  home-manager expire-generations "-${1:-$DEFAULT_MAX_AGE_DAYS} days"
-  nix-env --delete-generations "${1:-$DEFAULT_MAX_AGE_DAYS}d"
+  nix-update clean "$1"
 }
