d8ef66d867bb17a332106b5ceac67414e4332be0
Tom Payne <tom@tompayne.dev>
2022-07-28 23:26:25 +0100

    Update neotest symbols

--- dot_config/nvim/lua/tap/plugins/neotest.lua
+++ dot_config/nvim/lua/tap/plugins/neotest.lua
@@ -1,6 +1,15 @@
+local utils = require 'tap.utils'
+
 require('neotest').setup {
   discovery = {
     enabled = false,
+  },
+  icons = {
+    failed = utils.lsp_symbols.error,
+    passed = utils.lsp_symbols.ok,
+    running = '',
+    skipped = 'ﰸ',
+    unknown = '',
   },
   adapters = {
     require 'neotest-jest' {
