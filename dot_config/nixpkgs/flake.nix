18a607088934199c307b4ea302c79ec7e3c4ce96
Tom Payne <tom@tompayne.dev>
2022-12-07 09:23:38 +0000

    Update flake to 22.11 and convert MBP to new API

--- dot_config/nixpkgs/flake.nix
+++ dot_config/nixpkgs/flake.nix
@@ -2,7 +2,7 @@
   description = "Home-manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # TODO: Fix this (don't forget the overlay)
@@ -11,7 +11,7 @@
     nixgl.url = "github:guibou/nixGL";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -37,7 +37,7 @@
           system = "x86_64-linux";
           homeDirectory = "/home/tapayne88";
           username = "tapayne88";
-          stateVersion = "22.05";
+          stateVersion = "22.11";
           configuration = { pkgs, ... }:
             {
               nixpkgs.overlays = overlays;
@@ -53,11 +53,8 @@
         };
         # MacBook Pro (Work)
         "tom.payne@C02G41YZMD6R" = inputs.home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-darwin";
-          homeDirectory = "/Users/tom.payne";
-          username = "tom.payne";
-          stateVersion = "22.05";
-          configuration = { pkgs, ... }:
+          pkgs = inputs.nixpkgs.legacyPackages.${"x86_64-darwin"};
+          modules = [
             {
               nixpkgs.overlays = overlays;
               nixpkgs.config.allowUnfree = true;
@@ -68,14 +65,22 @@
                 ./modules/neovim.nix
                 ./modules/work.nix
               ];
-            };
+            }
+            {
+              home = {
+                username = "tom.payne";
+                homeDirectory = "/Users/tom.payne";
+                stateVersion = "22.11";
+              };
+            }
+          ];
         };
         # WSL
         "tpayne@DESKTOP-EACCNGB" = inputs.home-manager.lib.homeManagerConfiguration {
           system = "x86_64-linux";
           homeDirectory = "/home/tpayne";
           username = "tpayne";
-          stateVersion = "22.05";
+          stateVersion = "22.11";
           configuration = { pkgs, ... }:
             {
               nixpkgs.overlays = overlays;
