a6d07423dcfbb83a164b081a334995d80667afc3
Tom Payne <tom@tompayne.dev>
2023-01-19 17:43:05 +0000

    Added focus.nvim for better window management

--- dot_config/nvim/lua/tap/plugins/init.lua
+++ dot_config/nvim/lua/tap/plugins/init.lua
@@ -128,6 +128,19 @@ return require('packer').startup {
       },
     }
 
+    use {
+      'beauwilliams/focus.nvim',
+      config = function()
+        require('focus').setup {
+          signcolumn = false,
+          excluded_filetypes = { 'fugitive', 'git' },
+        }
+        require('tap.utils').keymap('n', '<leader>ft', ':FocusToggle<CR>', {
+          description = '[Focus] Toggle window focusing',
+        })
+      end,
+    }
+
     -- Simple plugin to easily resize windows
     use {
       'simeji/winresizer',
