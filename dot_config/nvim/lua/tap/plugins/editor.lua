0af09ba530b270cf60455e126c55ddaf089be863
Tom Payne <tom@tompayne.dev>
2023-01-22 15:25:43 +0000

    Moved toggleterm and lazy loaded

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -133,4 +133,26 @@ return {
       '[[',
     },
   },
+
+  -- persistent terminals
+  {
+    'akinsho/toggleterm.nvim',
+    cmd = 'ToggleTerm',
+    keys = { '<leader>tt' },
+    config = function()
+      local tmap = require('tap.utils').tmap
+
+      require('toggleterm').setup {
+        open_mapping = [[<leader>tt]],
+        insert_mappings = false,
+        shade_terminals = false,
+      }
+
+      -- Utilise tmux.nvim's bindings for changing windows
+      tmap('<C-h>', [[<C-\><C-n><C-h>]])
+      tmap('<C-j>', [[<C-\><C-n><C-j>]])
+      tmap('<C-k>', [[<C-\><C-n><C-k>]])
+      tmap('<C-l>', [[<C-\><C-n><C-l>]])
+    end,
+  },
 }
