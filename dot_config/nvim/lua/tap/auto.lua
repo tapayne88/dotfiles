497a3e73988d7904feba2fc8ba91ccf5286827c1
Tom Payne <tom@tompayne.dev>
2023-01-23 13:36:22 +0000

    Added util to automatically disable search highlight

--- dot_config/nvim/lua/tap/auto.lua
+++ dot_config/nvim/lua/tap/auto.lua
@@ -19,3 +19,17 @@ augroup('TapMkViews', {
     command = 'silent! loadview',
   },
 })
+
+-- Automatically disable search highlight done searching
+local function toggle_hlsearch(char)
+  if vim.fn.mode() == 'n' then
+    local keys = { '<CR>', 'n', 'N', '*', '#', '?', '/' }
+    local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
+
+    if vim.opt.hlsearch:get() ~= new_hlsearch then
+      vim.opt.hlsearch = new_hlsearch
+    end
+  end
+end
+
+vim.on_key(toggle_hlsearch, vim.api.nvim_create_namespace 'toggle_hlsearch')
