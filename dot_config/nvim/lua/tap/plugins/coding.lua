f6d19a57c986c42b5c03588ea171e36de81156a9
Tom Payne <tom@tompayne.dev>
2023-03-26 15:03:54 +0100

    Moved dap config to debugging lazy module

--- dot_config/nvim/lua/tap/plugins/coding.lua
+++ dot_config/nvim/lua/tap/plugins/coding.lua
@@ -156,137 +156,6 @@ return {
   },
 
   {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-    },
-    lazy = true,
-    init = function()
-      require('tap.utils').command {
-        'DapUi',
-        function()
-          require('dapui').toggle()
-        end,
-      }
-    end,
-    opts = {
-      icons = {
-        collapsed = '',
-        current_frame = '',
-        expanded = '',
-      },
-    },
-  },
-
-  {
-    'mxsdev/nvim-dap-vscode-js',
-    lazy = true,
-    dependencies = {
-      'microsoft/vscode-js-debug',
-      -- Pinned to version as build step was refactored and output changed (also
-      -- doesn't seem to work)
-      commit = '581f6451f6b5ed187ffa579623df19ff82d1476f',
-      build = 'npm install --legacy-peer-deps && npm run compile',
-    },
-    config = function()
-      local log_file_path = vim.fn.stdpath 'cache' .. '/dap_vscode_js.log' -- Path for file logging
-      require('tap.utils').notify_in_debug(
-        log_file_path,
-        vim.log.levels.DEBUG,
-        { title = 'nvim-dap' }
-      )
-
-      require('dap-vscode-js').setup {
-        -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-        debugger_path = require('lazy.core.config').options.root
-          .. '/vscode-js-debug', -- Path to vscode-js-debug installation.
-        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { 'pwa-node' }, -- which adapters to register in nvim-dap
-        log_file_path = log_file_path, -- Path for file logging
-        log_file_level = require('tap.utils').debug_enabled()
-            and vim.log.levels.DEBUG
-          or vim.log.levels.WARN, -- Logging level for output to file. Set to false to disable file logging.
-      }
-    end,
-  },
-
-  {
-    'mfussenegger/nvim-dap',
-    lazy = true,
-    dependencies = {
-      'mxsdev/nvim-dap-vscode-js',
-      { 'theHamsta/nvim-dap-virtual-text', config = true },
-      'hrsh7th/nvim-cmp',
-      'rcarriga/cmp-dap',
-    },
-    init = function()
-      require('tap.utils').command {
-        'DapToggleBreakpoint',
-        function()
-          require('dap').toggle_breakpoint()
-        end,
-      }
-      require('tap.utils').command {
-        'DapConditionalBreakpoint',
-        function()
-          require('dap').set_breakpoint(vim.fn.input 'Breakpoint Condition: ')
-        end,
-      }
-      require('tap.utils').command {
-        'DapLogPoint',
-        function()
-          require('dap').set_breakpoint(
-            nil,
-            nil,
-            vim.fn.input 'Log point message: '
-          )
-        end,
-      }
-    end,
-    config = function()
-      vim.fn.sign_define('DapBreakpoint', {
-        text = '⬤ ',
-        texthl = 'DiagnosticSignError',
-        linehl = '',
-        numhl = '',
-      })
-      vim.fn.sign_define('DapBreakpointCondition', {
-        text = '⬤ ',
-        texthl = 'DiagnosticSignWarn',
-        linehl = '',
-        numhl = '',
-      })
-      vim.fn.sign_define('DapLogPoint', {
-        text = '⬤ ',
-        texthl = 'DiagnosticSignHint',
-        linehl = '',
-        numhl = '',
-      })
-      vim.fn.sign_define('DapBreakpointRejected', {
-        text = '',
-        texthl = 'DiagnosticSignError',
-        linehl = '',
-        numhl = '',
-      })
-
-      require('cmp').setup {
-        enabled = function()
-          return require 'cmp.config.default'().enabled()
-            or require('cmp_dap').is_dap_buffer()
-        end,
-      }
-      require('cmp').setup.filetype(
-        { 'dap-repl', 'dapui_watches', 'dapui_hover' },
-        {
-          sources = {
-            { name = 'dap' },
-          },
-        }
-      )
-    end,
-  },
-
-  {
     'David-Kunz/jester',
     lazy = true,
     dependencies = {
@@ -379,11 +248,11 @@ return {
             -- 1. Find nearest package.json
             local file_dir = vim.fn.expand '%:p:h'
             local package_json_filename = 'package.json'
-            local cwd = require('tap.utils').root_pattern {
+            local package_json_dir = require('tap.utils').root_pattern {
               package_json_filename,
             }(file_dir)
 
-            if not cwd then
+            if not package_json_dir then
               vim.notify(
                 'Could not find package.json for file path ' .. file_dir,
                 vim.log.levels.INFO,
@@ -393,7 +262,9 @@ return {
             end
 
             -- 2. Parse package.json
-            local package_json_filepath = cwd .. '/' .. package_json_filename
+            local package_json_filepath = package_json_dir
+              .. '/'
+              .. package_json_filename
             local package_json_content =
               require('tap.utils').read_file(package_json_filepath)
 
@@ -426,7 +297,8 @@ return {
             )
 
             -- 4. Convert executable to .js source file
-            local script_tokens = get_runtime_args(test_script, cwd)
+            local script_tokens =
+              get_runtime_args(test_script, package_json_dir)
 
             if script_tokens == nil then
               vim.notify(
@@ -476,7 +348,7 @@ return {
                   env = script_tokens.env,
                   sourceMaps = true,
                   rootPath = '${workspaceFolder}',
-                  cwd = cwd,
+                  cwd = package_json_dir,
                   console = 'integratedTerminal',
                   internalConsoleOptions = 'neverOpen',
                 },
