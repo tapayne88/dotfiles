7373a4ccb44fd04fd2dbff39b29ab641e22ac96a
Tom Payne <tom@tompayne.dev>
2023-04-09 10:57:21 +0100

    Suppress lua-ls prompts for third party modules

--- dot_config/nvim/lua/tap/plugins/lsp/servers/lua_ls.lua
+++ dot_config/nvim/lua/tap/plugins/lsp/servers/lua_ls.lua
@@ -6,7 +6,17 @@ M.ensure_installed = { 'lua_ls' }
 
 function M.setup()
   require('neodev').setup {}
-  require('lspconfig').lua_ls.setup(lsp_utils.merge_with_default_config())
+  require('lspconfig').lua_ls.setup(lsp_utils.merge_with_default_config {
+    settings = {
+      Lua = {
+        -- Prevent annoying 'Do you need to configure your work environment as `X`?' prompts
+        -- According to https://github.com/LuaLS/lua-language-server/issues/679
+        -- neovim can't persist these settings so just gets annoying
+        ['runtime.version'] = 'LuaJIT',
+        ['workspace.checkThirdParty'] = false,
+      },
+    },
+  })
 end
 
 return M
