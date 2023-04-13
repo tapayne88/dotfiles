78b6c6a2bd211e66d5e15dd483c805a645396dee
Tom Payne <tom@tompayne.dev>
2023-04-13 10:31:32 +0100

    Added first attempt at statuscol config

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -525,4 +525,30 @@ return {
       vim.opt.undofile = true
     end,
   },
+
+  {
+    'luukvbaal/statuscol.nvim',
+    config = function()
+      local builtin = require 'statuscol.builtin'
+      require('statuscol').setup {
+        relculright = true,
+        segments = {
+          { text = { builtin.foldfunc } },
+          {
+            sign = { name = { 'Diagnostic' }, maxwidth = 1 },
+            click = 'v:lua.ScSa',
+          },
+          { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+          {
+            sign = { name = { 'GitSigns*' }, colwidth = 1 },
+            click = 'v:lua.ScSa',
+          },
+          {
+            sign = { name = { '.*' }, maxwidth = 2, colwidth = 1 },
+            click = 'v:lua.ScSa',
+          },
+        },
+      }
+    end,
+  },
 }
