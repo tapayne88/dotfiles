0d1235c750f7e738fd82e51a2808f5c1e3a2bade
Tom Payne <tom@tompayne.dev>
2023-04-13 13:02:10 +0100

    Added catppuccin neovim theme

--- dot_config/nvim/lua/tap/plugins/colorscheme.lua
+++ dot_config/nvim/lua/tap/plugins/colorscheme.lua
@@ -1,36 +1,40 @@
 -- treesitter colorschemes
 return {
-  'shaunsingh/nord.nvim',
-  lazy = false,
-  dependencies = {
-    'folke/tokyonight.nvim', -- light theme
-    'nvim-lua/plenary.nvim',
-    'rktjmp/fwatch.nvim', -- Utility for watching files
+  { 'catppuccin/nvim', name = 'catppuccin' },
+
+  {
+    'shaunsingh/nord.nvim',
+    lazy = false,
+    dependencies = {
+      'folke/tokyonight.nvim', -- light theme
+      'nvim-lua/plenary.nvim',
+      'rktjmp/fwatch.nvim', -- Utility for watching files
+    },
+    config = function()
+      local theme = require 'tap.utils.theme'
+
+      theme.set_colorscheme(theme.get_term_theme, { announce = false })
+      theme.setup_theme_watch()
+
+      require('tap.utils').apply_user_highlights(
+        'Theme',
+        function(highlight, color)
+          highlight('Search', {
+            guibg = color { dark = 'nord9_gui', light = 'blue2' },
+            guifg = color { dark = 'nord0_gui', light = 'bg' },
+            gui = 'NONE',
+          })
+          highlight('IncSearch', {
+            guibg = color { dark = 'nord9_gui', light = 'blue2' },
+            guifg = color { dark = 'nord0_gui', light = 'bg' },
+            gui = 'NONE',
+          })
+          highlight('FloatBorder', {
+            guifg = color { dark = 'nord9_gui', light = 'blue0' },
+            guibg = color { dark = 'nord0_gui', light = 'none' },
+          })
+        end
+      )
+    end,
   },
-  config = function()
-    local theme = require 'tap.utils.theme'
-
-    theme.set_colorscheme(theme.get_term_theme, { announce = false })
-    theme.setup_theme_watch()
-
-    require('tap.utils').apply_user_highlights(
-      'Theme',
-      function(highlight, color)
-        highlight('Search', {
-          guibg = color { dark = 'nord9_gui', light = 'blue2' },
-          guifg = color { dark = 'nord0_gui', light = 'bg' },
-          gui = 'NONE',
-        })
-        highlight('IncSearch', {
-          guibg = color { dark = 'nord9_gui', light = 'blue2' },
-          guifg = color { dark = 'nord0_gui', light = 'bg' },
-          gui = 'NONE',
-        })
-        highlight('FloatBorder', {
-          guifg = color { dark = 'nord9_gui', light = 'blue0' },
-          guibg = color { dark = 'nord0_gui', light = 'none' },
-        })
-      end
-    )
-  end,
 }
