a84b4481e5d89abfde0b821a6ebefb70d54edd2c
Tom Payne <tom@tompayne.dev>
2023-04-23 23:24:59 +0100

    Add colorizer to visualise colors in hex, rgb, etc.

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -580,4 +580,59 @@ return {
       }
     end,
   },
+
+  {
+    'NvChad/nvim-colorizer.lua',
+    lazy = true,
+    init = function()
+      -- Prevent loading of builtin colorizer commands
+      vim.g.loaded_colorizer = true
+
+      local opts = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = false, -- "Name" codes like Blue or blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        AARRGGBB = false, -- 0xAARRGGBB hex codes
+        rgb_fn = false, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = 'background', -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = false, -- Enable tailwind colors
+        -- parsers can contain values used in |user_default_options|
+        sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+        virtualtext = '■',
+        -- update color values even if buffer is not focused
+        -- example use: cmp_menu, cmp_docs
+        always_update = false,
+      }
+
+      require('tap.utils').command {
+        'ColorizerToggle',
+        function()
+          local colorizer = require 'colorizer'
+
+          if colorizer.is_buffer_attached(0) then
+            colorizer.detach_from_buffer(0)
+            vim.notify(
+              'Disabled colorizing for buffer',
+              vim.log.levels.INFO,
+              { title = 'Colorizer' }
+            )
+          else
+            colorizer.attach_to_buffer(0, opts)
+            vim.notify(
+              'Enabled colorizing for buffer',
+              vim.log.levels.INFO,
+              { title = 'Colorizer' }
+            )
+          end
+        end,
+      }
+    end,
+  },
 }
