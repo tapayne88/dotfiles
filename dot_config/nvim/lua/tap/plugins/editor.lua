6123cbcbeaf8d133331f4cce4a0c21525bb50324
Tom Payne <tom@tompayne.dev>
2023-05-09 17:49:35 +0100

    Update neo-tree icons inline with docs

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -238,8 +238,6 @@ return {
         },
         default_component_configs = {
           icon = {
-            folder_closed = '',
-            folder_open = '',
             folder_empty = '󰜌',
             folder_empty_open = '󰜌',
           },
@@ -257,6 +255,26 @@ return {
               staged = '',
               conflict = '',
             },
+          },
+        },
+        document_symbols = {
+          kinds = {
+            File = { icon = '󰈙', hl = 'Tag' },
+            Namespace = { icon = '󰌗', hl = 'Include' },
+            Package = { icon = '󰏖', hl = 'Label' },
+            Class = { icon = '󰌗', hl = 'Include' },
+            Property = { icon = '󰆧', hl = '@property' },
+            Enum = { icon = '󰒻', hl = '@number' },
+            Function = { icon = '󰊕', hl = 'Function' },
+            String = { icon = '󰀬', hl = 'String' },
+            Number = { icon = '󰎠', hl = 'Number' },
+            Array = { icon = '󰅪', hl = 'Type' },
+            Object = { icon = '󰅩', hl = 'Type' },
+            Key = { icon = '󰌋', hl = '' },
+            Struct = { icon = '󰌗', hl = 'Type' },
+            Operator = { icon = '󰆕', hl = 'Operator' },
+            TypeParameter = { icon = '󰊄', hl = 'Type' },
+            StaticMethod = { icon = '󰠄 ', hl = 'Function' },
           },
         },
         filesystem = {
