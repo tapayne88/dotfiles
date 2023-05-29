8983ff994955829a6fbfa98747674c0430f71636
Tom Payne <tom@tompayne.dev>
2023-05-29 11:31:07 +0100

    Change oil.nvim keymaps for splitting
    Also updated buffers settings to appear in the buffer list

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -211,10 +211,31 @@ return {
     'stevearc/oil.nvim',
     -- don't lazy load so it works when opening directories with `nvim .`
     lazy = false,
-    opts = { columns = {
-      'icon',
-      'permissions',
-    } },
+    opts = {
+      columns = {
+        'icon',
+        'permissions',
+      },
+      buf_options = {
+        buflisted = true,
+        bufhidden = 'hide',
+      },
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-v>'] = 'actions.select_vsplit',
+        ['<C-s>'] = 'actions.select_split',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['g.'] = 'actions.toggle_hidden',
+      },
+    },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     init = function()
       require('tap.utils').keymap(
