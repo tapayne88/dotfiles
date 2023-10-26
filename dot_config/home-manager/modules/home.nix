63197bac9ab10449e816e3a20dcb015759440074
Tom Payne <tom@tompayne.dev>
2023-10-26 16:13:44 +0100

    Add iperf to nixpkgs

--- dot_config/home-manager/modules/home.nix
+++ dot_config/home-manager/modules/home.nix
@@ -24,6 +24,7 @@
     gti # alias for git, with a gti
     htop # pretty top
     inetutils # network utils, like traceroute, etc.
+    iperf # Tool to measure IP bandwidth using UDP or TCP
     jq # json cli processor
     kubie # even nicer interaction with k8s cli with multiple configs
     mosh # better ssh
