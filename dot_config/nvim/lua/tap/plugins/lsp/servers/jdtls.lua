dffac8dade8246a4fde5b0ba39eaf53948f73142
Tom Payne <tom@tompayne.dev>
2024-01-15 09:49:23 +0000

    Add default LSP config to jdtls and extra config

--- dot_config/nvim/lua/tap/plugins/lsp/servers/jdtls.lua
+++ dot_config/nvim/lua/tap/plugins/lsp/servers/jdtls.lua
@@ -84,7 +84,10 @@ local lsp_settings = {
   contentProvider = {
     preferred = 'fernflower',
   },
-  extendedClientCapabilities = { resolveAdditionalTextEditsSupport = true },
+  extendedClientCapabilities = {
+    resolveAdditionalTextEditsSupport = true,
+    classFileContentsSupport = true,
+  },
   sources = {
     organizeImports = {
       starThreshold = 9999,
@@ -108,30 +111,32 @@ function M.setup()
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
   local jdtls_setup = function()
-    require('jdtls').start_or_attach {
-      cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-javaagent:' .. java_agent,
-        '-Xmx1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens',
-        'java.base/java.util=ALL-UNNAMED',
-        '--add-opens',
-        'java.base/java.lang=ALL-UNNAMED',
-        '-jar',
-        jdtls_install .. '/' .. jdtls_launcher,
-        '-configuration',
-        jdtls_install .. '/' .. config_name,
-        '-data',
-        vim.fn.stdpath 'cache' .. '/jdtls/workspace-root/' .. project_name,
-      },
-      settings = lsp_settings,
-    }
+    require('jdtls').start_or_attach(
+      require('tap.utils.lsp').merge_with_default_config {
+        cmd = {
+          'java',
+          '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+          '-Dosgi.bundles.defaultStartLevel=4',
+          '-Declipse.product=org.eclipse.jdt.ls.core.product',
+          '-Dlog.protocol=true',
+          '-Dlog.level=ALL',
+          '-javaagent:' .. java_agent,
+          '-Xmx1g',
+          '--add-modules=ALL-SYSTEM',
+          '--add-opens',
+          'java.base/java.util=ALL-UNNAMED',
+          '--add-opens',
+          'java.base/java.lang=ALL-UNNAMED',
+          '-jar',
+          jdtls_install .. '/' .. jdtls_launcher,
+          '-configuration',
+          jdtls_install .. '/' .. config_name,
+          '-data',
+          vim.fn.stdpath 'cache' .. '/jdtls/workspace-root/' .. project_name,
+        },
+        settings = lsp_settings,
+      }
+    ) {}
   end
 
   require('tap.utils').augroup('TapNvim-jdtls', {
