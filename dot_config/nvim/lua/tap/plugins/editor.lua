fcd51fe5e02a190a87fb4f54b68ce7ea7568aa48
Tom Payne <tom@tompayne.dev>
2024-01-17 11:10:27 +0000

    Added rgflow for better project searching with rg

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -589,4 +589,21 @@ return {
       },
     },
   },
+
+  {
+    -- TODO: Decide to keep or bin this
+    'mangelozzi/rgflow.nvim',
+    opts = { -- Set the default rip grep flags and options for when running a search via
+      -- RgFlow. Once changed via the UI, the previous search flags are used for
+      -- each subsequent search (until Neovim restarts).
+      cmd_flags = '--smart-case --fixed-strings --ignore --max-columns 200',
+
+      -- Mappings to trigger RgFlow functions
+      default_trigger_mappings = true,
+      -- These mappings are only active when the RgFlow UI (panel) is open
+      default_ui_mappings = true,
+      -- QuickFix window only mapping
+      default_quickfix_mappings = true,
+    },
+  },
 }
