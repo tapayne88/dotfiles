9df249821289b5e8b770e66cab110f6ad96848c8
Tom Payne <tom@tompayne.dev>
2024-02-16 10:54:12 +0000

    Fix gx.nvim config so it works

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -557,9 +557,14 @@ return {
 
   {
     'chrishrb/gx.nvim',
-    event = { 'BufEnter' },
+    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
+    cmd = { 'Browse' },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = true,
+    config = true, -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
   },
 
   {
