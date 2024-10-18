ce001bef6425a3c74bbef4f8ccdd1ec4e92482e9
Tom Payne <tom@tompayne.dev>
2024-10-18 17:58:35 +0100

    Swapped to different markdown renderer
    Mainly to allow disabling on the current line without which editing is a
    little tricky

--- dot_config/nvim/lua/tap/plugins/ui.lua
+++ dot_config/nvim/lua/tap/plugins/ui.lua
@@ -208,11 +208,14 @@ return {
   },
 
   {
-    'OXY2DEV/markview.nvim',
+    'MeanderingProgrammer/render-markdown.nvim',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
       'nvim-tree/nvim-web-devicons',
     },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 
   {
