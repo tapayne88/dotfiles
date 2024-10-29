7f2e6f95f1ab0d0aafca3f5c8e105a8f7c8b6387
Tom Payne <tom@tompayne.dev>
2024-10-29 11:10:12 +0000

    Fix treesitter and illuminate

--- dot_config/nvim/lua/tap/plugins/editor.lua
+++ dot_config/nvim/lua/tap/plugins/editor.lua
@@ -198,20 +198,14 @@ return {
   {
     'RRethy/vim-illuminate',
     event = 'BufReadPost',
-    opts = { delay = 200 },
-    init = function()
+    config = function()
+      require('illuminate').configure { delay = 200 }
+
       -- stylua: ignore start
       vim.keymap.set('n', ']]', function() require('illuminate').goto_next_reference(false) end, { desc = 'Next Reference' })
       vim.keymap.set('n', '[[', function() require('illuminate').goto_prev_reference(false) end, { desc = 'Prev Reference' })
       -- stylua: ignore end
     end,
-    config = function(_, opts)
-      require('illuminate').configure(opts)
-    end,
-    keys = {
-      ']]',
-      '[[',
-    },
   },
 
   -- persistent terminals
