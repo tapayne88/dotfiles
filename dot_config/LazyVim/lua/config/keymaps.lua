5d5f7045851bdcdd893d80db32407f68db7b27eb
Tom Payne <tom@tompayne.dev>
2024-12-21 01:07:45 +0000

    Add chezmoi keymap & fix fugitive

--- dot_config/LazyVim/lua/config/keymaps.lua
+++ dot_config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cM", ":!chezmoi apply -v<CR>", { desc = "Apply chezmoi changes" })
--- dot_config/LazyVim/lua/plugins/fugitive.lua
+++ dot_config/LazyVim/lua/plugins/fugitive.lua
@@ -18,15 +18,15 @@ return {
     "tpope/vim-rhubarb", -- :GBrowse github
     "shumphrey/fugitive-gitlab.vim", -- :GBrowse gitlab
   },
-  init = function()
-      -- stylua: ignore start
-      vim.keymap.set('n', '<leader>ga', ':Git add %:p<CR><CR>',       { desc = 'Git add file' })
-      vim.keymap.set('n', '<leader>gs', ':Git<CR>',                   { desc = 'Git status' })
-      vim.keymap.set('n', '<leader>gc', ':Git commit -v -q<CR>',      { desc = 'Git commit' })
-      vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<CR>',           { desc = 'Git diff' })
-      vim.keymap.set('n', '<leader>go', ':Git checkout<Space>',       { desc = 'Git checkout' })
+  keys = {
+    -- stylua: ignore start
+    { "<leader>ga", ":Git add %:p<CR><CR>",  desc = "Git add file" },
+    { "<leader>gs", ":Git<CR>",              desc = "Git status" },
+    { "<leader>gc", ":Git commit -v -q<CR>", desc = "Git commit" },
+    { "<leader>gd", ":Gvdiffsplit<CR>",      desc = "Git diff" } ,
+    { "<leader>go", ":Git checkout<Space>",  desc = "Git checkout" },
     -- stylua: ignore end
-  end,
+  },
   config = function()
     vim.g.fugitive_dynamic_colors = 0
     -- vim.g.github_enterprise_urls is set in .vimrc.local
