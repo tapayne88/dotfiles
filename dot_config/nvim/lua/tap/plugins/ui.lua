ca60196d6ff9be6fc3d6c3be24114a71e943e4a7
Tom Payne <tom@tompayne.dev>
2024-12-31 11:18:55 +0000

    feat: add animate plugin for nvim scrolling

--- dot_config/nvim/lua/tap/plugins/ui.lua
+++ dot_config/nvim/lua/tap/plugins/ui.lua
@@ -183,4 +183,16 @@ return {
     ---@type quicker.SetupOptions
     opts = {},
   },
+
+  {
+    'echasnovski/mini.animate',
+    event = 'VeryLazy',
+    opts = {
+      cursor = { enable = false },
+      scroll = { enable = true },
+      resize = { enable = false },
+      open = { enable = false },
+      close = { enable = false },
+    },
+  },
 }
