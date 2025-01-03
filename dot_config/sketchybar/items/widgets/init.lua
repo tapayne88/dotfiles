d2d25326ee916c0325bcdbc0801ba4ece308baa9
Tom Payne <tom@tompayne.dev>
2025-01-03 23:43:45 +0000

    feat: add wifi widget to sketchybar

--- dot_config/sketchybar/items/widgets/init.lua
+++ dot_config/sketchybar/items/widgets/init.lua
@@ -1,3 +1,4 @@
+require 'items.widgets.wifi'
 require 'items.widgets.battery'
 require 'items.widgets.volume'
 require 'items.widgets.cpu'
---
+++ dot_config/sketchybar/items/widgets/wifi.lua
@@ -0,0 +1,57 @@
+local settings = require 'settings'
+local colors = require 'colors'
+
+local wifi = sbar.add('item', 'widgets.wifi', {
+  position = 'right',
+  icon = {
+    font = {
+      style = settings.font.style_map['Regular'],
+      size = 14.0,
+    },
+    color = colors.bg1,
+    padding_left = 8,
+    padding_right = 6,
+  },
+  label = { padding_left = 0, padding_right = 0 },
+  update_freq = 180,
+  drawing = false,
+})
+
+local function update_vpn_status()
+  sbar.exec("scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1", function(ip_address)
+    sbar.exec(os.getenv 'HOME' .. '/.local/bin/check-vpn-connected', function(_, exit_code)
+      local color
+      local icon
+      local drawing = false
+
+      local is_vpn = exit_code == 0
+
+      if is_vpn then
+        color = colors.green
+        icon = '󱚿'
+        drawing = true
+      elseif ip_address ~= '' then
+        color = colors.blue
+        icon = '󰖩'
+        drawing = true
+      else
+        color = colors.white
+        icon = '󰖪'
+        drawing = true
+      end
+
+      wifi:set {
+        icon = {
+          string = icon,
+        },
+        background = {
+          color = color,
+        },
+        drawing = drawing,
+      }
+    end)
+  end)
+end
+
+update_vpn_status()
+wifi:subscribe({ 'wifi_change' }, update_vpn_status)
