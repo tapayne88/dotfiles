57df47744b9f223e90432899a79bf2a8e7bb5e98
Tom Payne <tom@tompayne.dev>
2025-01-05 00:15:57 +0000

    fix: fixed wifi widget

--- dot_config/sketchybar/items/widgets/wifi.lua
+++ dot_config/sketchybar/items/widgets/wifi.lua
@@ -12,48 +12,41 @@ local wifi = sbar.add('item', 'widgets.wifi', {
     color = colors.bg1,
     padding_left = 8,
     padding_right = 8,
+    drawing = false,
   },
-  label = { padding_left = 0, padding_right = 0 },
+  label = { drawing = false },
   update_freq = 180,
-  drawing = false,
 })
 
-local function update_vpn_status()
+wifi:subscribe({ 'routine', 'wifi_change' }, function()
   sbar.exec("scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1", function(ip_address)
     sbar.exec(os.getenv 'HOME' .. '/.local/bin/check-vpn-connected', function(_, exit_code)
       local color
       local icon
-      local drawing = false
 
       local is_vpn = exit_code == 0
 
       if is_vpn then
         color = colors.palette.green
         icon = icons.wifi.vpn
-        drawing = true
       elseif ip_address ~= '' then
         color = colors.palette.blue
         icon = icons.wifi.connected
-        drawing = true
       else
         color = colors.white
         icon = '󰖪'
         icon = icons.wifi.disconnected
-        drawing = true
       end
 
       wifi:set {
         icon = {
           string = icon,
+          drawing = true,
         },
         background = {
           color = color,
         },
-        drawing = drawing,
       }
     end)
   end)
-end
-
-update_vpn_status()
-wifi:subscribe({ 'wifi_change' }, update_vpn_status)
+end)
