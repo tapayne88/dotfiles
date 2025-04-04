f0c40a4a067dbee6b5a3a9f2b39e2a7d0a133712
Tom Payne <tom@tompayne.dev>
2025-04-04 17:59:34 +0100

    fix: format in old node repos

--- dot_config/nvim/lua/tap/plugins/lsp/init.lua
+++ dot_config/nvim/lua/tap/plugins/lsp/init.lua
@@ -144,49 +144,52 @@ return {
   {
     {
       'stevearc/conform.nvim',
-      opts = function()
-        local js_ts_formatters = {
-          'prettierd',
-          'prettier',
-          stop_after_first = true,
-        }
+      config = function()
+        require('plenary.async').run(function()
+          local js_ts_formatters = {
+            'prettierd',
+            'prettier',
+            stop_after_first = true,
+          }
 
-        require('tap.utils.lsp').on_attach(function(_, bufnr)
-          require('tap.utils').keymap({ 'n', 'v' }, '<space>f', function()
-            require('conform').format { bufnr = bufnr }
-          end, { buffer = bufnr, desc = '[LSP] Run formatter' })
-        end)
+          require('tap.utils.lsp').on_attach(function(_, bufnr)
+            require('tap.utils').keymap({ 'n', 'v' }, '<space>f', function()
+              require('conform').format { bufnr = bufnr }
+            end, { buffer = bufnr, desc = '[LSP] Run formatter' })
+          end)
 
-        -- local asdf_nodejs_global_version = require('tap.utils.async').get_asdf_global_version 'nodejs'
-        return {
-          formatters = {
-            sqlfluff = {
-              args = { 'format', '--dialect', 'mysql', '-' },
-              cwd = require('conform.util').root_file { '.editorconfig', 'package.json' },
-            },
-            prettierd = {
-              env = {
-                CWD = vim.fn.stdpath 'data',
+          local asdf_nodejs_global_version = require('tap.utils.async').get_asdf_global_version 'nodejs'
+          vim.schedule(function()
+            require('conform').setup {
+              formatters = {
+                sqlfluff = {
+                  args = { 'format', '--dialect', 'mysql', '-' },
+                  cwd = require('conform.util').root_file { '.editorconfig', 'package.json' },
+                },
+                prettierd = {
+                  env = {
+                    ASDF_NODEJS_VERSION = asdf_nodejs_global_version,
+                  },
+                },
               },
-            },
-          },
 
-          formatters_by_ft = {
-            javascript = js_ts_formatters,
-            javascriptreact = js_ts_formatters,
-            lua = { 'stylua' },
-            mysql = { 'sqlfluff' },
-            sql = { 'sqlfluff' },
-            typescript = js_ts_formatters,
-            typescriptreact = js_ts_formatters,
-          },
+              formatters_by_ft = {
+                javascript = js_ts_formatters,
+                javascriptreact = js_ts_formatters,
+                lua = { 'stylua' },
+                mysql = { 'sqlfluff' },
+                sql = { 'sqlfluff' },
+                typescript = js_ts_formatters,
+                typescriptreact = js_ts_formatters,
+              },
 
-          format_after_save = {
-            -- timeout_ms = 500,
-            async = true,
-            lsp_format = 'fallback',
-          },
-        }
+              format_after_save = {
+                async = true,
+                lsp_format = 'fallback',
+              },
+            }
+          end)
+        end, require('tap.utils').noop)
       end,
     },
   },
