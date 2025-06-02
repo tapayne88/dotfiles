3c9d50117ccb2abd6ed7f05d545295192b85ab13
Tom Payne <tom@tompayne.dev>
2025-06-02 11:57:52 +0100

    chore: copy lazy vtsls config

--- dot_config/nvim/lua/tap/plugins/lsp/servers/vtsls.lua
+++ dot_config/nvim/lua/tap/plugins/lsp/servers/vtsls.lua
@@ -3,7 +3,35 @@ local lsp_utils = require 'tap.utils.lsp'
 M.ensure_installed = { 'vtsls' }
 
 function M.setup()
-  require('lspconfig').vtsls.setup(lsp_utils.merge_with_default_config {})
+  require('lspconfig').vtsls.setup(lsp_utils.merge_with_default_config {
+    settings = {
+      complete_function_calls = true,
+      vtsls = {
+        enableMoveToFileCodeAction = true,
+        autoUseWorkspaceTsdk = true,
+        experimental = {
+          maxInlayHintLength = 30,
+          completion = {
+            enableServerSideFuzzyMatch = true,
+          },
+        },
+      },
+      typescript = {
+        updateImportsOnFileMove = { enabled = 'always' },
+        suggest = {
+          completeFunctionCalls = true,
+        },
+        inlayHints = {
+          enumMemberValues = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          parameterNames = { enabled = 'literals' },
+          parameterTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          variableTypes = { enabled = false },
+        },
+      },
+    },
+  })
 end
 
 return M
