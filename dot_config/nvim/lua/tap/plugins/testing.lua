1b1a06cb3797a6f430d278dd87ca8a985a5edc72
Tom Payne <tom@tompayne.dev>
2025-07-22 10:10:59 +0100

    feat: update trouble quickfix on test result

--- dot_config/nvim/lua/tap/plugins/testing.lua
+++ dot_config/nvim/lua/tap/plugins/testing.lua
@@ -32,6 +32,34 @@ return {
             require('trouble').open { mode = 'quickfix', focus = false }
           end,
         },
+        consumers = {
+          -- Update trouble quickfix on test result
+          trouble = function(client)
+            client.listeners.results = function(adapter_id, results, partial)
+              if partial then
+                return
+              end
+              local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+              local failed = 0
+              for pos_id, result in pairs(results) do
+                if result.status == 'failed' and tree:get_key(pos_id) then
+                  failed = failed + 1
+                end
+              end
+              vim.schedule(function()
+                local trouble = require 'trouble'
+                if trouble.is_open() then
+                  trouble.refresh()
+                  if failed == 0 then
+                    trouble.close()
+                  end
+                end
+              end)
+              return {}
+            end
+          end,
+        },
         adapters = {
           require 'neotest-vitest' {
             cwd = function(testFilePath)
