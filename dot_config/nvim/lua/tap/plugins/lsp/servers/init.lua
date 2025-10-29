b2a1befc4537e75e3b6feb6e1a5e3c7eda784dd3
Tom Payne <tom@tompayne.dev>
2025-10-29 09:30:26 +0000

    chore: disable tailwindcss since no longer using it

--- dot_config/nvim/lua/tap/plugins/lsp/servers/init.lua
+++ dot_config/nvim/lua/tap/plugins/lsp/servers/init.lua
@@ -6,7 +6,8 @@ return {
   require 'tap.plugins.lsp.servers.jsonls',
   require 'tap.plugins.lsp.servers.jsonnet',
   require 'tap.plugins.lsp.servers.lua_ls',
-  require 'tap.plugins.lsp.servers.tailwindcss',
+  -- No longer using tailwindcss
+  -- require 'tap.plugins.lsp.servers.tailwindcss',
   require 'tap.plugins.lsp.servers.terraformls',
   require 'tap.plugins.lsp.servers.vtsls',
   require 'tap.plugins.lsp.servers.yamlls',
