4ea53fa12228bde2725dcfa76717d3b459957294
Tom Payne <tom@tompayne.dev>
2025-12-07 15:25:54 +0000

    feat: add ghostty to crostini

--- dot_config/home-manager/modules/crostini.nix
+++ dot_config/home-manager/modules/crostini.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     unstable.alacritty # alacritty terminal
     unstable.kitty # kitty terminal
+    unstable.ghostty # Fast, native, feature-rich terminal emulator pushing modern features
     unstable.mesa-demos # utility for inspect openGL config
     nixgl.nixGLIntel # OpenGL wrapper for nix programs - Pixelbook specific
     unstable.obsidian # note taking app, see permittedInsecurePackages in flake
--- dot_config/zsh/functions/crostini.zsh
+++ dot_config/zsh/functions/crostini.zsh
@@ -5,3 +5,7 @@ function kitty {
 function glxinfo {
   nixGLIntel glxinfo "$@"
 }
+
+function ghostty {
+  nixGLIntel ghostty "$@"
+}
