25954f58573f308c9bc583ca12f206e33f0a4139
Tom Payne <tom@tompayne.dev>
2025-12-07 15:24:46 +0000

    fix: swap package for glx tools

--- dot_config/home-manager/modules/crostini.nix
+++ dot_config/home-manager/modules/crostini.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     unstable.alacritty # alacritty terminal
     unstable.kitty # kitty terminal
-    unstable.glxinfo # utility for inspect openGL config
+    unstable.mesa-demos # utility for inspect openGL config
     nixgl.nixGLIntel # OpenGL wrapper for nix programs - Pixelbook specific
     unstable.obsidian # note taking app, see permittedInsecurePackages in flake
     xsel # copy / paste from neovim
