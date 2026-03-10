64295d80e1b456e9d97095f9961ecf9b66597285
Tom Payne <tom.payne@anaplan.com>
2026-03-10 17:19:58 +0000

    chore: update gw script to support new worktree structure

--- dot_config/zsh/functions/git.zsh
+++ dot_config/zsh/functions/git.zsh
@@ -39,49 +39,6 @@ function git() {
   fi
 }
 
-function gw() {
-  if [ "$1" = "-h" ]; then
-    echo "gw - quick way to swap between git worktrees and remain in the same subdirectory, using fzf"
-    echo ""
-    echo "Usage:"
-    echo "   gw <flags> <worktree_name>"
-    echo ""
-    echo "Flags:"
-    echo "   -h     Help, print his help message"
-    echo ""
-    echo "\`gw\` will show available git worktrees via fzf."
-    echo "\`gw master\` will swap to the master worktree in the same subdirectory."
-    return
-  fi
-
-  if [ "$(git rev-parse --git-dir 2> /dev/null)" = "" ]; then
-    echo "not a git repository"
-    return 1
-  fi
-
-  GIT_PREFIX=$(git rev-parse --show-prefix)
-
-  if [ "$1" != "" ]; then
-    for WT in $(git worktree list | awk '{ print $1 }'); do
-      if [ "$(basename $WT)" = "$1" ]; then
-        cd "$WT/$GIT_PREFIX"
-        return
-      fi
-    done
-
-    echo "no worktree found called $1"
-    return 1
-  else
-    WORKTREE_DIR=$(git worktree list | fzf --height 15 --header "swap worktree" | awk '{ print $1 }')
-
-    if [ "$WORKTREE_DIR" = "" ]; then
-      return 1
-    fi
-
-    cd "$WORKTREE_DIR/$GIT_PREFIX"
-  fi
-}
-
 function git-worktree-init() {
   if [ "$1" = "-h" ]; then
     echo "git-worktree-init - convert a git repo to worktree setup"
@@ -114,13 +71,14 @@ function git-worktree-init() {
 
   # Make new directory structure
   echo "setting up new directory structure"
-  mkdir -p "$REPO_NAME/worktrees"
-  cp -r "$TEMP_REPO/.git" "$REPO_NAME/.$REPO_NAME.git"
+  mkdir -p "$REPO_NAME"
+  cp -r "$TEMP_REPO/.git" "$REPO_NAME/.git"
 
   # Setup hidden actual repo as bare to avoid accidentally checking out a branch
   echo "setting hidden repo as bare"
-  cd "$REPO_NAME/.$REPO_NAME.git"
+  cd "$REPO_NAME/.git"
   git config --bool core.bare true
 
-  echo "run \`git worktree add ../worktrees/<worktree_name> <branch>\` to add a worktree"
+  echo "you're ready to start managing your worktrees with worktrunk"
+  echo "https://worktrunk.dev"
 }
