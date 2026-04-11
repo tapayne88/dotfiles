4720a82f859d00f2b06c64b7b0498a89f2a10a9c
Tom Payne <tom@tompayne.dev>
2026-04-11 17:22:32 +0100

    chore: tidy up flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,9 +1,5 @@
 {
   inputs = {
-    # This is pointing to an unstable release.
-    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
-    # i.e. nixos-24.11
-    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
@@ -20,8 +16,6 @@
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-wallpaper.url = "github:lunik1/nix-wallpaper";
   };
   outputs = inputs@{ self, nixpkgs, home-manager, stylix, ... }: {
     nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
