ca64c15316ec77fdede9c3deb40989178619b7c9
Tom Payne <tom@tompayne.dev>
2026-04-12 16:26:34 +0100

    chore: update packages

--- nixos/modules/shell.nix
+++ nixos/modules/shell.nix
@@ -23,17 +23,23 @@
     antigen # zsh plugin manager
     atuin # Magical shell history
     bat # colourised cat
+    chezmoi # dotfile manager
+    curl # curl...
     delta # better git diffs
+    dig # dns lookup
+    duf # pretty alternative to df
     eza # ls replacement
     fd # faster more user friendly find
     fzf # fuzzy-finder
     gti # alias for git, with a gti
+    just # Handy way to save and run project-specific commands
     jq # json cli processor
     ripgrep # rg searching
     vivid # A themeable LS_COLORS generator with a rich filetype datebase
     zoxide # A smarter cd command
 
     # unstable
+    _1password-cli
     carapace # A multi-shell completion library
     git # newest git!
     jqp # TUI playground for jq
@@ -41,6 +47,7 @@
     kubie # even nicer interaction with k8s cli with multiple configs
     lazygit # simple terminal UI for git commands
     television # A very fast, portable and hackable fuzzy finder.
+    tldr # simplified and community-driven man pages
     tmux # terminal multiplexer
   ];
 }
