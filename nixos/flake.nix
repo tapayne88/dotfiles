9148e05d96101f0a2625840c9a4c6ff3ef3064f5
Tom Payne <tom@tompayne.dev>
2026-04-12 15:48:45 +0100

    chore: move home-manager configs to nixos flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
-  outputs = inputs@{ self, nixpkgs-unstable, home-manager, stylix, ... }: {
+  outputs = inputs@{ self, nixpkgs-stable, nixpkgs-unstable, home-manager, stylix, ... }: {
     nixosConfigurations.thinkpad = nixpkgs-unstable.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
@@ -46,6 +46,49 @@
         }
         stylix.nixosModules.stylix
       ];
+    };
+    homeConfigurations = {
+      # Pixelbook Omarchy
+      "tpayne@omarchy-pixelbook" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-stable.legacyPackages.x86_64-linux;
+        modules = [
+          {
+            imports = [
+              ./modules/shell.nix
+              ./modules/linux.nix
+              ./modules/neovim.nix
+            ];
+          }
+          {
+            home = {
+              username = "tpayne";
+              homeDirectory = "/home/tpayne";
+              stateVersion = "25.11";
+            };
+          }
+        ];
+      };
+      # MacBook Pro M3 (Work)
+      "tom.payne@KL2M3W1G4N" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-stable.legacyPackages.aarch64-darwin;
+        modules = [
+          {
+            imports = [
+              ./modules/shell.nix
+              ./modules/darwin.nix
+              ./modules/neovim.nix
+              ./modules/work.nix
+            ];
+          }
+          {
+            home = {
+              username = "tom.payne";
+              homeDirectory = "/Users/tom.payne";
+              stateVersion = "25.11";
+            };
+          }
+        ];
+      };
     };
   };
 }
