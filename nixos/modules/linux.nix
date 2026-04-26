d7ec3be3263878fafe3b57bef4e5e070bc67553a
Tom Payne <tom@tompayne.dev>
2026-04-26 23:29:27 +0100

    chore: move wlogout config to nix

--- nixos/modules/linux.nix
+++ nixos/modules/linux.nix
@@ -52,4 +52,58 @@
         on-timeout = systemctl suspend                # suspend pc
     }
   '';
+
+  xdg.configFile."wlogout/style.css".text = ''
+    * {
+        background-image: none;
+        box-shadow: none;
+    }
+
+    window {
+        background-color: rgba(30, 30, 46, 0.90);
+    }
+
+    button {
+        border-radius: 0;
+        border-color: #89b4fa;
+        text-decoration-color: #cdd6f4;
+        color: #cdd6f4;
+        background-color: #181825;
+        border-style: solid;
+        border-width: 1px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 25%;
+    }
+
+    button:focus, button:active, button:hover {
+        /* 20% Overlay 2, 80% mantle */
+        background-color: rgb(48, 50, 66);
+        outline-style: none;
+    }
+
+    #lock {
+        background-image: url("${../home/wlogout/icons/lock.svg}");
+    }
+
+    #logout {
+        background-image: url("${../home/wlogout/icons/logout.svg}");
+    }
+
+    #suspend {
+        background-image: url("${../home/wlogout/icons/suspend.svg}");
+    }
+
+    #hibernate {
+        background-image: url("${../home/wlogout/icons/hibernate.svg}");
+    }
+
+    #shutdown {
+        background-image: url("${../home/wlogout/icons/shutdown.svg}");
+    }
+
+    #reboot {
+        background-image: url("${../home/wlogout/icons/reboot.svg}");
+    }
+  '';
 }
