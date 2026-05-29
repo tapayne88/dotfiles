449447c523a20b2f7f16c67842d64379584a5722
Tom Payne <tom@tompayne.dev>
2026-05-29 12:06:57 +0100

    feat: add font config to stylix

--- nixos/hosts/common.nix
+++ nixos/hosts/common.nix
@@ -29,6 +29,27 @@
       size = 24;
     };
 
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
+      };
+
+      sansSerif = {
+        package = pkgs.inter;
+        name = "Inter";
+      };
+
+      serif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
