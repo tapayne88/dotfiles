1d3d7b865b444db7d89cfb4532ad18c53fe52837
Tom Payne <tom@tompayne.dev>
2026-06-21 00:10:08 +0100

    fix: nemo icon in vicinae

--- nixos/nixosModules/file-browser.nix
+++ nixos/nixosModules/file-browser.nix
@@ -4,7 +4,23 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    nemo
+    # Fix nemo icon
+    (symlinkJoin {
+      name = "nemo";
+      paths = [ nemo ];
+      postBuild = ''
+        # Remove the symlinked desktop file from the cache clone
+        rm $out/share/applications/nemo.desktop
+
+        # Copy the raw text file from the binary cache so we can edit it
+        cp ${nemo}/share/applications/nemo.desktop $out/share/applications/nemo.desktop
+
+        # Make it writable and swap out the icon string
+        chmod +w $out/share/applications/nemo.desktop
+        substituteInPlace $out/share/applications/nemo.desktop \
+          --replace "Icon=system-file-manager" "Icon=nemo"
+      '';
+    })
     nemo-with-extensions
   ];
 }
