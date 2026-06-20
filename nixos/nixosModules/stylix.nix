a7f2eaa48b75db9a4051a3f390956e0974f86eab
Tom Payne <tom@tompayne.dev>
2026-06-21 00:18:14 +0100

    chore: update wallpaper

--- nixos/nixosModules/stylix.nix
+++ nixos/nixosModules/stylix.nix
@@ -3,7 +3,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ../home/catppuccin-nix.png;
+    image = ../assets/nixos-catppuccin-mocha.png;
     polarity = "dark";
 
     cursor = {
