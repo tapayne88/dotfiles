1f82b1d6a3ad3b840502dd67abd982ea2203766e
Tom Payne <tom@tompayne.dev>
2026-06-27 12:44:51 +0100

    feat: add btop

--- nixos/homeManagerModules/shell.nix
+++ nixos/homeManagerModules/shell.nix
@@ -54,4 +54,11 @@
     clean.enable = true;
     clean.extraArgs = "--keep-since 7d --keep 3";
   };
+
+  programs.btop = {
+    enable = true;
+    settings = {
+      vim_keys = true;
+    };
+  };
 }
