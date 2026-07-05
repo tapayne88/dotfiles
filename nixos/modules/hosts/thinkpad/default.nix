f5b0a4fe648bf963d2f7542cb8862f49d4dbb28e
Tom Payne <tom@tompayne.dev>
2026-07-05 18:18:00 +0100

    fix: remove default.nix files

--- nixos/modules/hosts/thinkpad/default.nix
+++ nixos/modules/hosts/thinkpad/default.nix
@@ -29,7 +29,20 @@
 
       # ../../configs/host-options.nix
       # ../../configs/nixpkgs.nix
-      self.modules.nixos.default
+      self.modules.nixos.audio
+      self.modules.nixos.darktable
+      self.modules.nixos.file-browser
+      self.modules.nixos.file-syncing
+      self.modules.nixos.greeter
+      self.modules.nixos.home-manager
+      self.modules.nixos.network-shares
+      self.modules.nixos.tailscale
+      self.modules.nixos.password-manager
+      self.modules.nixos.printing
+      self.modules.nixos.programs
+      self.modules.nixos.stylix
+      self.modules.nixos.user
+      self.modules.nixos.window-manager
       self.modules.nixos.thinkpadConfiguration
     ];
   };
--- nixos/modules/nixosModules/home-manager.nix
+++ nixos/modules/nixosModules/home-manager.nix
@@ -11,7 +11,14 @@
         };
         users."${config.hostSettings.username}" = {
           imports = [
-            self.modules.homeManager.default
+            self.modules.homeManager.neovim
+            self.modules.homeManager.shell
+            self.modules.homeManager.linux
+            self.modules.homeManager.browser
+            self.modules.homeManager.hyprland
+            self.modules.homeManager.obsidian
+            self.modules.homeManager.programs
+            self.modules.homeManager.vicinae
           ];
           home = {
             username = config.hostSettings.username;
