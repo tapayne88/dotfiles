d161442043330cebb71de9f631b854bbcd625790
Tom Payne <tom@tompayne.dev>
2026-07-14 23:13:02 +0100

    refactor: make disko config more readable

--- nixos/modules/features/disko.nix
+++ nixos/modules/features/disko.nix
@@ -28,72 +28,71 @@
           main = {
             device = config.hostSettings.mainDevice;
             type = "disk";
-            content = {
-              type = "gpt";
-              partitions = {
-                # Boot partition
-                ESP = {
-                  type = "EF00";
-                  size = config.hostSettings.bootSize;
-                  content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = "/boot";
-                    mountOptions = [
-                      "fmask=0022"
-                      "dmask=0022"
-                    ];
-                  };
+            content.type = "gpt";
+
+            # Boot partition
+            content.partitions.ESP = {
+              type = "EF00";
+              size = config.hostSettings.bootSize;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0022"
+                  "dmask=0022"
+                ];
+              };
+            };
+
+            # Encrypted partition
+            content.partitions.luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                settings = {
+                  allowDiscards = true;
                 };
 
-                # Encrypted partition
-                luks = {
-                  size = "100%";
-                  content = {
-                    type = "luks";
-                    name = "cryptroot";
-                    settings = {
-                      allowDiscards = true;
-                    };
-                    content = {
-                      type = "btrfs";
-                      extraArgs = [ "-f" ];
-                      subvolumes = {
-                        "@nix" = {
-                          mountpoint = nixMountPath;
-                          mountOptions = [
-                            "compress=zstd"
-                            "noatime"
-                          ];
-                        };
-                        "@persist" = {
-                          mountpoint = config.hostSettings.persistenceMountPath;
-                          mountOptions = [
-                            "compress=zstd"
-                            "noatime"
-                          ];
-                        };
-                        "@log" = {
-                          mountpoint = logMountPath;
-                          mountOptions = [
-                            "compress=zstd"
-                            "noatime"
-                          ];
-                        };
-                        "@swap" = {
-                          mountpoint = swapMountPath;
-                          mountOptions = [ "noatime" ];
-                          swap = {
-                            swapfile = {
-                              size = config.hostSettings.swapSize;
-                            };
-                          };
-                        };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+
+                  subvolumes."@nix" = {
+                    mountpoint = nixMountPath;
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+
+                  subvolumes."@persist" = {
+                    mountpoint = config.hostSettings.persistenceMountPath;
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+
+                  subvolumes."@log" = {
+                    mountpoint = logMountPath;
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+
+                  subvolumes."@swap" = {
+                    mountpoint = swapMountPath;
+                    mountOptions = [ "noatime" ];
+                    swap = {
+                      swapfile = {
+                        size = config.hostSettings.swapSize;
                       };
                     };
                   };
                 };
-
               };
             };
           };
